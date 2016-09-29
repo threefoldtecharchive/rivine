NebulousLabs/Sia

add some startup debugging output to stdout in tpool

modules/transactionpool/persist.go
@@ -2,9 +2,11 @@ package transactionpool
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 
+	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/persist"
 	"github.com/NebulousLabs/Sia/types"
@@ -53,6 +55,9 @@ func (tp *TransactionPool) initPersist() error {
 	}
 
 	// Open the database file.
+	if build.DEBUG && build.Release != "testing" {
+		fmt.Println("\tTransaction Pool: opening database")
+	}
 	tp.db, err = persist.OpenDatabase(dbMetadata, filepath.Join(tp.persistDir, dbFilename))
 	if err != nil {
 		return err
@@ -85,10 +90,16 @@ func (tp *TransactionPool) initPersist() error {
 	}
 
 	// Subscribe to the consensus set using the most recent consensus change.
+	if build.DEBUG && build.Release != "testing" {
+		fmt.Println("\tTransaction Pool: subscribing to the consensus set")
+	}
 	err = tp.consensusSet.ConsensusSetSubscribe(tp, cc)
 	if err == modules.ErrInvalidConsensusChangeID {
 		// Reset and rescan because the consensus set does not recognize the
 		// provided consensus change id.
+		if build.DEBUG && build.Release != "testing" {
+			fmt.Println("\tTransaction Pool: subscription failed, resetting and resubscribing")
+		}
 		resetErr := tp.db.Update(func(tx *bolt.Tx) error {
 			return tp.resetDB(tx)
 		})

modules/transactionpool/transactionpool.go
@@ -2,9 +2,11 @@ package transactionpool
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/NebulousLabs/demotemutex"
 
+	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/crypto"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/persist"
@@ -102,9 +104,12 @@ func New(cs modules.ConsensusSet, g modules.Gateway, persistDir string) (*Transa
 	if err != nil {
 		return nil, err
 	}
+	if build.DEBUG && build.Release != "testing" {
+		fmt.Println("\tTransaction Pool: persist initialization complete")
+	}
 
 	// Register RPCs
-	g.RegisterRPC("RelayTransactionSet", tp.relayTransactionSet)
+	go g.RegisterRPC("RelayTransactionSet", tp.relayTransactionSet)
 
 	return tp, nil
 }

threefoldtecharchive/rivine

modules/transactionpool/persist.go
@@ -2,9 +2,11 @@ package transactionpool
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 
+	"github.com/rivine/rivine/build"
 	"github.com/rivine/rivine/modules"
 	"github.com/rivine/rivine/persist"
 	"github.com/rivine/rivine/types"
@@ -53,6 +55,9 @@ func (tp *TransactionPool) initPersist() error {
 	}
 
 	// Open the database file.
+	if build.DEBUG && build.Release != "testing" {
+		fmt.Println("\tTransaction Pool: opening database")
+	}
 	tp.db, err = persist.OpenDatabase(dbMetadata, filepath.Join(tp.persistDir, dbFilename))
 	if err != nil {
 		return err
@@ -85,10 +90,16 @@ func (tp *TransactionPool) initPersist() error {
 	}
 
 	// Subscribe to the consensus set using the most recent consensus change.
+	if build.DEBUG && build.Release != "testing" {
+		fmt.Println("\tTransaction Pool: subscribing to the consensus set")
+	}
 	err = tp.consensusSet.ConsensusSetSubscribe(tp, cc)
 	if err == modules.ErrInvalidConsensusChangeID {
 		// Reset and rescan because the consensus set does not recognize the
 		// provided consensus change id.
+		if build.DEBUG && build.Release != "testing" {
+			fmt.Println("\tTransaction Pool: subscription failed, resetting and resubscribing")
+		}
 		resetErr := tp.db.Update(func(tx *bolt.Tx) error {
 			return tp.resetDB(tx)
 		})

modules/transactionpool/transactionpool.go
@@ -2,9 +2,11 @@ package transactionpool
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/NebulousLabs/demotemutex"
 
+	"github.com/rivine/rivine/build"
 	"github.com/rivine/rivine/crypto"
 	"github.com/rivine/rivine/modules"
 	"github.com/rivine/rivine/persist"
@@ -102,9 +104,12 @@ func New(cs modules.ConsensusSet, g modules.Gateway, persistDir string) (*Transa
 	if err != nil {
 		return nil, err
 	}
+	if build.DEBUG && build.Release != "testing" {
+		fmt.Println("\tTransaction Pool: persist initialization complete")
+	}
 
 	// Register RPCs
-	g.RegisterRPC("RelayTransactionSet", tp.relayTransactionSet)
+	go g.RegisterRPC("RelayTransactionSet", tp.relayTransactionSet)
 
 	return tp, nil
 }