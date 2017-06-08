805e96d065d81e5593cbe33cef9e83bd83c7c0f8
Rob Van Mieghem <rob@incubaid.com>
2017-06-08 15:28:23 +0200

    Modify the published archives folder structure and naming

--- publish.sh
+++ publish.sh
@@ -6,17 +6,18 @@ VERSION="$(git describe)"
 echo "Building version $VERSION"
 
 docker build -t rivinebuilder .
-docker run --rm -v "$PWD":/go/src/github.com/rivine/rivine --entrypoint sh rivinebuilder -c "GOOS=linux GOARCH=amd64 go build -ldflags '-s' -v -o dist/linux/rivined github.com/rivine/rivine/rivined && GOOS=linux GOARCH=amd64 go build -ldflags '-s' -v -o dist/linux/rivinec github.com/rivine/rivine/rivinec"
-docker run --rm -v "$PWD":/go/src/github.com/rivine/rivine --entrypoint sh rivinebuilder -c "GOOS=windows GOARCH=amd64 go build -ldflags '-s' -v -o dist/windows/rivined.exe github.com/rivine/rivine/rivined && GOOS=windows GOARCH=amd64 go build -ldflags '-s' -v -o dist/windows/rivinec.exe github.com/rivine/rivine/rivinec"
-docker run --rm -v "$PWD":/go/src/github.com/rivine/rivine --entrypoint sh rivinebuilder -c "GOOS=darwin GOARCH=amd64 go build -ldflags '-s' -v -o dist/darwin/rivined github.com/rivine/rivine/rivined && GOOS=darwin GOARCH=amd64 go build -ldflags '-s' -v -o dist/darwin/rivinec github.com/rivine/rivine/rivinec"
-docker build -t rivine/rivine:"$VERSION" -f DockerfileMinimal .
+docker run --rm -v "$PWD":/go/src/github.com/rivine/rivine --entrypoint sh rivinebuilder -c "GOOS=linux GOARCH=amd64 go build -ldflags '-s' -v -o dist/rivine-$VERSION-linux-amd64/rivined github.com/rivine/rivine/rivined && GOOS=linux GOARCH=amd64 go build -ldflags '-s' -v -o dist/rivine-$VERSION-linux-amd64/rivinec github.com/rivine/rivine/rivinec"
+docker run --rm -v "$PWD":/go/src/github.com/rivine/rivine --entrypoint sh rivinebuilder -c "GOOS=windows GOARCH=amd64 go build -ldflags '-s' -v -o dist/rivine-$VERSION-windows-amd64/rivined.exe github.com/rivine/rivine/rivined && GOOS=windows GOARCH=amd64 go build -ldflags '-s' -v -o dist/rivine-$VERSION-windows-amd64/rivinec.exe github.com/rivine/rivine/rivinec"
+docker run --rm -v "$PWD":/go/src/github.com/rivine/rivine --entrypoint sh rivinebuilder -c "GOOS=darwin GOARCH=amd64 go build -ldflags '-s' -v -o dist/rivine-$VERSION-darwin-amd64/rivined github.com/rivine/rivine/rivined && GOOS=darwin GOARCH=amd64 go build -ldflags '-s' -v -o dist/rivine-$VERSION-darwin-amd64/rivinec github.com/rivine/rivine/rivinec"
+
+docker build -t rivine/rivine:"$VERSION" -f DockerfileMinimal --build-arg binaries_location=dist/rivine-"$VERSION"-linux-amd64 .
 
 docker push rivine/rivine:"$VERSION"
 
 pushd dist
 
-zip -D rivine_"$VERSION"_windows.zip windows/*
-tar -c -z -f rivine_"$VERSION"_darwin.tar.gz darwin/*
-tar -c -z -f rivine_"$VERSION"_linux.tar.gz linux/*
+tar -c -z -f rivine-"$VERSION"-linux-amd64.tar.gz rivine-"$VERSION"-linux-amd64
+zip rivine-"$VERSION"-windows-amd64.zip rivine-"$VERSION"-windows-amd64/*
+zip rivine-"$VERSION"-darwin-amd64.zip rivine-"$VERSION"-darwin-amd64/*
 
-popd
+popd dist
