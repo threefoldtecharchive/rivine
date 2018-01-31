04fd668f36d8dd6e451b00c29cae0b845a3b2f3f
Lee Smet <lee.smet@hotmail.com>
2018-01-31 14:36:19 +0100

    Gateway running with exposed api endpoints

--- doc/examples/offline_transaction/deploy_sample_setup.sh
+++ doc/examples/offline_transaction/deploy_sample_setup.sh
@@ -8,7 +8,7 @@ genesisoutputseed="across knife thirsty puck itches hazard enmity fainted pebble
 # First ensure that we have the testnet dockers build
 docker build -t rivine_testnet ../../../. -f ./Dockerfile_testnet
 
-# Now run 2 which will create the blocks
+# Now run 2 which will (well could, only one will have the blockstakes to do so) create the blocks
 docker run -d --name r1 rivine_testnet
 docker run -d --name r2 rivine_testnet
 
@@ -18,11 +18,18 @@ r2_addr=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" r2)
 # Connect the dockers
 docker exec r1 rivinec gateway connect "$r2_addr:23112"
  
-echo "$testpass"; echo "$genesisoutputseed"
 
 # Create a wallet
 echo "$testpass" | docker exec -i r1 rivinec wallet init -p
 echo "$testpass" | docker exec -i r1 rivinec wallet unlock
+
+echo "$testpass" | docker exec -i r2 rivinec wallet init -p
+echo "$testpass" | docker exec -i r2 rivinec wallet unlock
+
+# Save an address for later
+addr=$(docker exec r2 rivinec wallet address)
+# Trim the "Created new address: prefix so we only have the hash"
+addr=${addr#"Created new address: "}
 
 # Load the seed
 docker exec -i r1 rivinec wallet load seed << EOF
@@ -38,17 +45,20 @@ echo "$testpass" | docker exec -i r1 rivinec wallet unlock
 
 # Start a gateway daemon, without wallet or blockcreator modules
 
-# Funny story: if we specify `--entrypoint "rivined ..." with all the required arguments, the `rivined` process will spawn as the init process
-# in the docker (PID 1). Which seems to prevent it from binding ports, thus doing anything usefull... Whoever would have known.
-# This also applies to the way it is currently specified, so i guess this should be fixed at some point 
-# docker run -d -i --name r3 rivine_testnet --disable-api-security --authenticate-api --no-bootstrap -M cgte << EOF
-# $testpass
-# EOF
+# So piping the password to the docker to ensure that the http api listens on none localhost addresses causes some issues.
+docker run -d -i --name r3 rivine_testnet --disable-api-security --authenticate-api --no-bootstrap -M cgte --api-addr :23110
+# Do some serious monkey business to get the gateway running
+echo $testpass | docker attach r3
+
+
+# Give the gateway some time to initialize
+sleep 1
 
 # Connect the gateway to the network
-# docker exec r3 rivinec gateway connect "$r1_addr:23112"
+echo $testpass | docker exec -i r3 rivinec gateway connect "$r1_addr:23112"
 
 # So now we have a docker with a gateway running which accepts commands from non-localhost addresses.
 # Echo the gateway ip for good measure
-# echo "gateway ip:"
-# echo $(docker inspect -f "{{ .NetworkSettings.IPAddress }}" r3)
+echo "Gateway addr:"
+echo http://$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" r3):23110
+echo "Possible address to send coins: $addr"
