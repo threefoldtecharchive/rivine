b2cb18fde0fceaccfb4503958fb8c4844fac050c
Lee Smet <lee.smet@hotmail.com>
2018-02-02 15:06:33 +0100

    Update docker deploy script in example to connect with container name

--- doc/examples/offline_transaction/deploy_sample_setup.sh
+++ doc/examples/offline_transaction/deploy_sample_setup.sh
@@ -4,19 +4,30 @@ set -e
 
 testpass="test123"
 genesisoutputseed="across knife thirsty puck itches hazard enmity fainted pebbles unzip echo queen rarest aphid bugs yanks okay abbey eskimos dove orange nouns august ailments inline rebel glass tyrant acumen"
+network_name="rivnet"
 
-# First ensure that we have the testnet dockers build
+# Check if the test network exists and create it if it does not
+docker network inspect $network_name &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "Network does not exist yet, create it"
+    docker network create $network_name
+else 
+    echo "Network already exists, skip creation"
+fi
+
+# Ensure that we have the testnet dockers build
 docker build -t rivine_testnet ../../../. -f ./Dockerfile_testnet
 
+
 # Now run 2 which will (well could, only one will have the blockstakes to do so) create the blocks
-docker run -d --name r1 rivine_testnet
-docker run -d --name r2 rivine_testnet
+docker run -d --name r1 --net=$network_name rivine_testnet
+docker run -d --name r2 --net=$network_name rivine_testnet
 
 r1_addr=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" r1)
 r2_addr=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" r2)
 
 # Connect the dockers
-docker exec r1 rivinec gateway connect "$r2_addr:23112"
+docker exec r1 rivinec gateway connect "r2:23112"
  
 
 # Create a wallet
@@ -46,7 +57,7 @@ echo "$testpass" | docker exec -i r1 rivinec wallet unlock
 # Start a gateway daemon, without wallet or blockcreator modules
 
 # So piping the password to the docker to ensure that the http api listens on none localhost addresses causes some issues.
-docker run -d -i --name r3 rivine_testnet --disable-api-security --authenticate-api --no-bootstrap -M cgte --api-addr :23110
+docker run -d -i --name r3 --net=$network_name rivine_testnet --disable-api-security --authenticate-api --no-bootstrap -M cgte --api-addr :23110
 # Do some serious monkey business to get the gateway running
 echo $testpass | docker attach r3
 
@@ -55,10 +66,10 @@ echo $testpass | docker attach r3
 sleep 1
 
 # Connect the gateway to the network
-echo $testpass | docker exec -i r3 rivinec gateway connect "$r1_addr:23112"
+echo $testpass | docker exec -i r3 rivinec gateway connect "r1.$network_name:23112"
 
 # So now we have a docker with a gateway running which accepts commands from non-localhost addresses.
 # Echo the gateway ip for good measure
 echo "Gateway addr:"
-echo http://$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" r3):23110
+echo http://$(docker inspect -f "{{ .NetworkSettings.Networks.$network_name.IPAddress }}" r3):23110
 echo "Possible address to send coins: $addr"
