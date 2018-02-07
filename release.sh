6cc8973f925ee743b74bba3cb03efd494cbb37bf
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2018-02-07 22:32:53 +0100

    use git tag and commit as version for release builds

--- release.sh
+++ release.sh
@@ -5,18 +5,19 @@ set -e
 #  + add validation...
 #  + integrate gitian (https://gitian.org/)?
 
+# TODO
+# + Use git describe as default option? (tag or commit or both?)
+# + inject the version + commit into build/version.go
+
 # version is supplied as argument
-version="$1"
-if [ -z "$version" ]; then
-    echo "no version specified"
-    echo "usage: $0 version"
-    exit 1
-fi
+package="github.com/rivine/rivine"
+full_version=$(git describe | cut -d '-' -f 1,3)
+version=$(echo "$full_version" | cut -d '-' -f 1)
 
 for os in darwin linux windows; do
 	echo Packaging ${os}...
 	# create workspace
-	folder="release/rivine-$version-$os-amd64"
+	folder="release/rivine-${version}-${os}-amd64"
 	rm -rf "$folder"
 	mkdir -p "$folder"
 	# compile and sign binaries
@@ -25,13 +26,16 @@ for os in darwin linux windows; do
 		if [ "$os" == "windows" ]; then
 			bin=${pkg}.exe
 		fi
-		GOOS=${os} go build -a -tags 'netgo' -ldflags="-s -w" -o "$folder/$bin" "./$pkg"
+		GOOS=${os} go build -a -tags 'netgo' \
+			-ldflags="-X ${package}/build.rawVersion=${full_version} -s -w" \
+			-o "${folder}/${bin}" "./${pkg}"
 
 	done
 	# add other artifacts
 	cp -r doc LICENSE README.md "$folder"
 	# zip
 	(
-		zip -rq "release/rivine-$version-$os-amd64.zip" "release/rivine-$version-$os-amd64"
+		zip -rq "release/rivine-${version}-${os}-amd64.zip" \
+			"release/rivine-${version}-${os}-amd64"
 	)
 done
