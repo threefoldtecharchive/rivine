c68186056b7b7be523ab1f2c1b83e24cdc91e87e
LeeSmet <lee.smet@hotmail.com>
2018-02-20 17:41:00 +0100

    Explicitly set the client name for rivine

--- cmd/rivinec/main.go
+++ cmd/rivinec/main.go
@@ -3,5 +3,7 @@ package main
 import "github.com/rivine/rivine/rivinec"
 
 func main() {
+	// The name defaults to rivine if it isn't specified but set it again to make sure
+	rivinec.ClientName = "rivine"
 	rivinec.DefaultClient()
 }
