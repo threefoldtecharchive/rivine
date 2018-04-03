f711d703cdb4aea09674d6baf9355054e6ddeecc
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2018-04-03 13:32:05 +0200

    remove update endpoints in rivined
    fixes #236

--- pkg/daemon/server.go
+++ pkg/daemon/server.go
@@ -1,28 +1,17 @@
 package daemon
 
 import (
-	"archive/zip"
-	"bytes"
-	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"math/big"
 	"net"
 	"net/http"
-	"path"
-	"path/filepath"
-	"runtime"
 	"strings"
 
 	"github.com/rivine/rivine/api"
 	"github.com/rivine/rivine/build"
 	"github.com/rivine/rivine/types"
 
-	"github.com/inconshreveable/go-update"
 	"github.com/julienschmidt/httprouter"
-	"github.com/kardianos/osext"
 )
 
 var errEmptyUpdateResponse = errors.New("API call to https://api.github.com/repos/rivine/rivine/releases/latest is returning an empty response")
@@ -83,172 +72,6 @@ type (
 	}
 )
 
-const (
-	// The developer key is used to sign updates and other important Sia-
-	// related information.
-	developerKey = `-----BEGIN PUBLIC KEY-----
-NOT USED FOR NOW
------END PUBLIC KEY-----`
-)
-
-// fetchLatestRelease returns metadata about the most recent GitHub release.
-func fetchLatestRelease() (githubRelease, error) {
-	req, err := http.NewRequest("GET", "https://api.github.com/repos/rivine/rivine/releases/latest", nil)
-	if err != nil {
-		return githubRelease{}, err
-	}
-	req.Header.Set("Accept", "application/vnd.github.v3+json")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return githubRelease{}, err
-	}
-	defer resp.Body.Close()
-	var release githubRelease
-	err = json.NewDecoder(resp.Body).Decode(&release)
-	if err != nil {
-		return githubRelease{}, err
-	}
-	if release.TagName == "" && len(release.Assets) == 0 {
-		return githubRelease{}, errEmptyUpdateResponse
-	}
-	return release, nil
-}
-
-// updateToRelease updates siad and siac to the release specified. siac is
-// assumed to be in the same folder as siad.
-func updateToRelease(release githubRelease) error {
-	updateOpts := update.Options{
-		Verifier: update.NewRSAVerifier(),
-	}
-	err := updateOpts.SetPublicKeyPEM([]byte(developerKey))
-	if err != nil {
-		// should never happen
-		return err
-	}
-
-	binaryFolder, err := osext.ExecutableFolder()
-	if err != nil {
-		return err
-	}
-
-	// construct release filename
-	releaseName := fmt.Sprintf("Rivine-%s-%s-%s.zip", release.TagName, runtime.GOOS, runtime.GOARCH)
-
-	// find release
-	var downloadURL string
-	for _, asset := range release.Assets {
-		if asset.Name == releaseName {
-			downloadURL = asset.DownloadURL
-			break
-		}
-	}
-	if downloadURL == "" {
-		return errors.New("couldn't find download URL for " + releaseName)
-	}
-
-	// download release archive
-	resp, err := http.Get(downloadURL)
-	if err != nil {
-		return err
-	}
-	// release should be small enough to store in memory (<10 MiB); use
-	// LimitReader to ensure we don't download more than 32 MiB
-	content, err := ioutil.ReadAll(io.LimitReader(resp.Body, 1<<25))
-	resp.Body.Close()
-	if err != nil {
-		return err
-	}
-	r := bytes.NewReader(content)
-	z, err := zip.NewReader(r, r.Size())
-	if err != nil {
-		return err
-	}
-
-	// process zip, finding siad/siac binaries and signatures
-	for _, binary := range []string{"rivined", "rivinec"} {
-		var binData io.ReadCloser
-		var signature []byte
-		var binaryName string // needed for TargetPath below
-		for _, zf := range z.File {
-			switch base := path.Base(zf.Name); base {
-			case binary, binary + ".exe":
-				binaryName = base
-				binData, err = zf.Open()
-				if err != nil {
-					return err
-				}
-				defer binData.Close()
-			case binary + ".sig", binary + ".exe.sig":
-				sigFile, err := zf.Open()
-				if err != nil {
-					return err
-				}
-				defer sigFile.Close()
-				signature, err = ioutil.ReadAll(sigFile)
-				if err != nil {
-					return err
-				}
-			}
-		}
-		if binData == nil {
-			return errors.New("could not find " + binary + " binary")
-		} else if signature == nil {
-			return errors.New("could not find " + binary + " signature")
-		}
-
-		// apply update
-		updateOpts.Signature = signature
-		updateOpts.TargetMode = 0775 // executable
-		updateOpts.TargetPath = filepath.Join(binaryFolder, binaryName)
-		err = update.Apply(binData, updateOpts)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// daemonUpdateHandlerGET handles the API call that checks for an update.
-func (srv *Server) daemonUpdateHandlerGET(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
-	release, err := fetchLatestRelease()
-	if err != nil {
-		api.WriteError(w, api.Error{Message: "Failed to fetch latest release: " + err.Error()}, http.StatusInternalServerError)
-		return
-	}
-	latestVersion, err := build.Parse(release.TagName)
-	if err != nil {
-		api.WriteError(w, api.Error{Message: "Failed to parse latest release: " + err.Error()}, http.StatusInternalServerError)
-		return
-	}
-	api.WriteJSON(w, UpdateInfo{
-		Available: latestVersion.Compare(build.Version) > 0,
-		Version:   latestVersion.String(),
-	})
-}
-
-// daemonUpdateHandlerPOST handles the API call that updates siad and siac.
-// There is no safeguard to prevent "updating" to the same release, so callers
-// should always check the latest version via daemonUpdateHandlerGET first.
-// TODO: add support for specifying version to update to.
-func (srv *Server) daemonUpdateHandlerPOST(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
-	release, err := fetchLatestRelease()
-	if err != nil {
-		api.WriteError(w, api.Error{Message: "Failed to fetch latest release: " + err.Error()}, http.StatusInternalServerError)
-		return
-	}
-	err = updateToRelease(release)
-	if err != nil {
-		if rerr := update.RollbackError(err); rerr != nil {
-			api.WriteError(w, api.Error{Message: "Serious error: Failed to rollback from bad update: " + rerr.Error()}, http.StatusInternalServerError)
-		} else {
-			api.WriteError(w, api.Error{Message: "Failed to apply update: " + err.Error()}, http.StatusInternalServerError)
-		}
-		return
-	}
-	api.WriteSuccess(w)
-}
-
 // debugConstantsHandler prints a json file containing all of the constants.
 func (srv *Server) daemonConstantsHandler(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	sc := SiaConstants{
@@ -307,8 +130,6 @@ func (srv *Server) daemonHandler(password string) http.Handler {
 
 	router.GET("/daemon/constants", srv.daemonConstantsHandler)
 	router.GET("/daemon/version", srv.daemonVersionHandler)
-	router.GET("/daemon/update", srv.daemonUpdateHandlerGET)
-	router.POST("/daemon/update", srv.daemonUpdateHandlerPOST)
 	router.POST("/daemon/stop", api.RequirePassword(srv.daemonStopHandler, password))
 
 	return router
