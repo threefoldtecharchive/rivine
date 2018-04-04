NebulousLabs/Sia

repair TestShareNodes ndf

modules/gateway/nodes_test.go
@@ -1,11 +1,13 @@
 package gateway
 
 import (
+	"errors"
 	"strconv"
 	"sync"
 	"testing"
 	"time"
 
+	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/encoding"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/fastrand"
@@ -169,6 +171,19 @@ func TestShareNodes(t *testing.T) {
 	err = g1.Connect(g2.Address())
 	if err != nil {
 		t.Fatal("couldn't connect:", err)
+	}
+
+	err = build.Retry(50, 100*time.Millisecond, func() error {
+		g1.mu.Lock()
+		_, exists := g1.nodes[dummyNode]
+		g1.mu.Unlock()
+		if !exists {
+			return errors.New("node not added")
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	// g1 should have received the node

threefoldtecharchive/rivine

modules/gateway/nodes_test.go
@@ -1,12 +1,14 @@
 package gateway
 
 import (
+	"errors"
 	"strconv"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/NebulousLabs/fastrand"
+	"github.com/rivine/rivine/build"
 	"github.com/rivine/rivine/encoding"
 	"github.com/rivine/rivine/modules"
 )
@@ -169,6 +171,19 @@ func TestShareNodes(t *testing.T) {
 	err = g1.Connect(g2.Address())
 	if err != nil {
 		t.Fatal("couldn't connect:", err)
+	}
+
+	err = build.Retry(50, 100*time.Millisecond, func() error {
+		g1.mu.Lock()
+		_, exists := g1.nodes[dummyNode]
+		g1.mu.Unlock()
+		if !exists {
+			return errors.New("node not added")
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	// g1 should have received the node