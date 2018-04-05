fandikurnia/Sia

peer-to-peer ip discovery

modules/gateway/ip.go
@@ -1,0 +1,87 @@
+package gateway
+
+import (
+	"net"
+	"time"
+
+	"github.com/NebulousLabs/Sia/encoding"
+	"github.com/NebulousLabs/Sia/modules"
+	"github.com/NebulousLabs/errors"
+)
+
+// discoverPeerIP is the handler for the discoverPeer RPC. It returns the
+// public ip of the caller back to the caller. This allows for peer-to-peer ip
+// discovery without centralized services.
+func (g *Gateway) discoverPeerIP(conn modules.PeerConn) error {
+	conn.SetDeadline(time.Now().Add(connStdDeadline))
+	host, _, err := net.SplitHostPort(conn.RemoteAddr().String())
+	if err != nil {
+		return errors.AddContext(err, "failed to split host from port")
+	}
+	return encoding.WriteObject(conn, host)
+}
+
+// managedIPFromPeers asks the peers the node is connected to for the node's public ip address. If not enough peers are available
+func (g *Gateway) managedIPFromPeers() (string, error) {
+	for {
+		// Check for shutdown signal.
+		select {
+		case <-g.peerTG.StopChan():
+			return "", errors.New("interrupted by shutdown")
+		default:
+		}
+
+		// Get peers
+		g.mu.RLock()
+		peers := g.Peers()
+		g.mu.RUnlock()
+
+		// Check if there are enough peers. Otherwise wait.
+		if len(peers) < minPeersForIPDiscovery {
+			select {
+			case <-time.After(peerDiscoveryRetryInterval):
+			case <-g.peerTG.StopChan():
+			}
+			continue
+		}
+
+		// Ask all the peers about our ip in parallel
+		returnChan := make(chan modules.NetAddress)
+		for _, peer := range peers {
+			go g.RPC(peer.NetAddress, "DiscoverIP", func(conn modules.PeerConn) error {
+				var address string
+				err := encoding.ReadObject(conn, &address, 100)
+				returnChan <- modules.NetAddress(address)
+				return err
+			})
+		}
+		// Wait for their responses
+		addresses := make(map[string]int)
+		for i := 0; i < len(peers); i++ {
+			addr := <-returnChan
+			if addr.IsValid() == nil {
+				addresses[addr.Host()]++
+			}
+		}
+		// If there haven't been enough successful responses we wait some time.
+		if len(addresses) < minPeersForIPDiscovery {
+			select {
+			case <-time.After(peerDiscoveryRetryInterval):
+			case <-g.peerTG.StopChan():
+			}
+			continue
+		}
+		// If an address was returned by more than half the peers we consider
+		// it valid.
+		for addr, count := range addresses {
+			if count > len(addresses)/2 {
+				return addr, nil
+			}
+		}
+		// Otherwise we wait before trying again.
+		select {
+		case <-time.After(peerDiscoveryRetryInterval):
+		case <-g.peerTG.StopChan():
+		}
+	}
+}

modules/gateway/ip_test.go
@@ -1,0 +1,45 @@
+package gateway
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/NebulousLabs/Sia/encoding"
+	"github.com/NebulousLabs/Sia/modules"
+)
+
+// TestIpRPC tests the ip discovery RPC.
+func TestIpRPC(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	t.Parallel()
+
+	// Create gateways for testing.
+	g1 := newNamedTestingGateway(t, "1")
+	defer g1.Close()
+	g2 := newNamedTestingGateway(t, "2")
+	defer g2.Close()
+
+	// Connect gateways.
+	err := g1.Connect(g2.Address())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Call RPC
+	err = g1.RPC(g2.Address(), "DiscoverIP", func(conn modules.PeerConn) error {
+		var address string
+		err := encoding.ReadObject(conn, &address, 100)
+		if err != nil {
+			t.Error("failed to read object from response", err)
+		}
+		if address != g1.Address().Host() {
+			return fmt.Errorf("ip addresses don't match %v != %v", g1.Address().Host(), address)
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal("RPC failed", err)
+	}
+}

threefoldtecharchive/rivine

modules/gateway/ip.go
@@ -1,0 +1,87 @@
+package gateway
+
+import (
+	"net"
+	"time"
+
+	"github.com/NebulousLabs/errors"
+	"github.com/rivine/rivine/encoding"
+	"github.com/rivine/rivine/modules"
+)
+
+// discoverPeerIP is the handler for the discoverPeer RPC. It returns the
+// public ip of the caller back to the caller. This allows for peer-to-peer ip
+// discovery without centralized services.
+func (g *Gateway) discoverPeerIP(conn modules.PeerConn) error {
+	conn.SetDeadline(time.Now().Add(connStdDeadline))
+	host, _, err := net.SplitHostPort(conn.RemoteAddr().String())
+	if err != nil {
+		return errors.AddContext(err, "failed to split host from port")
+	}
+	return encoding.WriteObject(conn, host)
+}
+
+// managedIPFromPeers asks the peers the node is connected to for the node's public ip address. If not enough peers are available
+func (g *Gateway) managedIPFromPeers() (string, error) {
+	for {
+		// Check for shutdown signal.
+		select {
+		case <-g.peerTG.StopChan():
+			return "", errors.New("interrupted by shutdown")
+		default:
+		}
+
+		// Get peers
+		g.mu.RLock()
+		peers := g.Peers()
+		g.mu.RUnlock()
+
+		// Check if there are enough peers. Otherwise wait.
+		if len(peers) < minPeersForIPDiscovery {
+			select {
+			case <-time.After(peerDiscoveryRetryInterval):
+			case <-g.peerTG.StopChan():
+			}
+			continue
+		}
+
+		// Ask all the peers about our ip in parallel
+		returnChan := make(chan modules.NetAddress)
+		for _, peer := range peers {
+			go g.RPC(peer.NetAddress, "DiscoverIP", func(conn modules.PeerConn) error {
+				var address string
+				err := encoding.ReadObject(conn, &address, 100)
+				returnChan <- modules.NetAddress(address)
+				return err
+			})
+		}
+		// Wait for their responses
+		addresses := make(map[string]int)
+		for i := 0; i < len(peers); i++ {
+			addr := <-returnChan
+			if addr.IsValid() == nil {
+				addresses[addr.Host()]++
+			}
+		}
+		// If there haven't been enough successful responses we wait some time.
+		if len(addresses) < minPeersForIPDiscovery {
+			select {
+			case <-time.After(peerDiscoveryRetryInterval):
+			case <-g.peerTG.StopChan():
+			}
+			continue
+		}
+		// If an address was returned by more than half the peers we consider
+		// it valid.
+		for addr, count := range addresses {
+			if count > len(addresses)/2 {
+				return addr, nil
+			}
+		}
+		// Otherwise we wait before trying again.
+		select {
+		case <-time.After(peerDiscoveryRetryInterval):
+		case <-g.peerTG.StopChan():
+		}
+	}
+}

modules/gateway/ip_test.go
@@ -1,0 +1,45 @@
+package gateway
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/rivine/rivine/encoding"
+	"github.com/rivine/rivine/modules"
+)
+
+// TestIpRPC tests the ip discovery RPC.
+func TestIpRPC(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	t.Parallel()
+
+	// Create gateways for testing.
+	g1 := newNamedTestingGateway(t, "1")
+	defer g1.Close()
+	g2 := newNamedTestingGateway(t, "2")
+	defer g2.Close()
+
+	// Connect gateways.
+	err := g1.Connect(g2.Address())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Call RPC
+	err = g1.RPC(g2.Address(), "DiscoverIP", func(conn modules.PeerConn) error {
+		var address string
+		err := encoding.ReadObject(conn, &address, 100)
+		if err != nil {
+			t.Error("failed to read object from response", err)
+		}
+		if address != g1.Address().Host() {
+			return fmt.Errorf("ip addresses don't match %v != %v", g1.Address().Host(), address)
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal("RPC failed", err)
+	}
+}