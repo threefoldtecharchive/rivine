a6e8d6529451a61c3f9618a288aeff801e24a4de
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2018-04-26 08:47:37 +0200

    allow json-unmarshal of empty unlock hashes
    fixes #273

--- types/unlockhash_test.go
+++ types/unlockhash_test.go
@@ -63,7 +63,7 @@ func TestUnlockHashJSONMarshalling(t *testing.T) {
 	marUH[7] -= 100
 
 	// Try an input of the wrong length.
-	err = (&umarUH).UnmarshalJSON(marUH[2:])
+	err = (&umarUH).UnmarshalJSON([]byte(`"foo"`))
 	if err != ErrUnlockHashWrongLen {
 		t.Error("Got wrong error:", err)
 	}
@@ -259,5 +259,29 @@ func TestUnlockHashLoadString(t *testing.T) {
 	}
 	if bytes.Compare(hash[:], uh.Hash[:]) != 0 {
 		t.Fatal("loaded hash isn't correct:", hash, uh.Hash)
+	}
+}
+
+// test as part of fix for https://github.com/rivine/rivine/issues/273
+func TestUnlockHashLoadEmptyString(t *testing.T) {
+	var uh UnlockHash
+	err := uh.LoadString("")
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+	if uh.Cmp(UnlockHash{}) != 0 {
+		t.Fatal(uh, "!=", UnlockHash{})
+	}
+}
+
+// test as part of fix for https://github.com/rivine/rivine/issues/273
+func TestUnlockHashLoadEmptyJSONString(t *testing.T) {
+	var uh UnlockHash
+	err := uh.UnmarshalJSON([]byte(`""`))
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+	if uh.Cmp(UnlockHash{}) != 0 {
+		t.Fatal(uh, "!=", UnlockHash{})
 	}
 }
