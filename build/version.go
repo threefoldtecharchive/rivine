373e3517dc23f48bae617f52d6d8fca8705ddedd
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2018-05-09 07:44:12 +0200

    update rawVersion to 1.0.5

--- build/version.go
+++ build/version.go
@@ -133,7 +133,7 @@ var (
 
 var (
 	// rawVersion used to generate rivine's protocol version
-	rawVersion = "v1.0.5-alpha"
+	rawVersion = "v1.0.5"
 	// Version is the current version of rivined.
 	Version ProtocolVersion
 )
