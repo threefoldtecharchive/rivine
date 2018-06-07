threefoldtecharchive/rivine

commit 016931ead7f58333945bc8d01567fb8ea30e0444

make CLI client stateless by default
and fix he progress estimation percentage
(fixes threefoldfoundation/tfchain#105)

cmd/rivinec/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"github.com/rivine/rivine/pkg/client"
+	"github.com/rivine/rivine/types"
+	"github.com/threefoldfoundation/tfchain/pkg/config"
 )
 
 func main() {
-	client.DefaultCLIClient(client.DefaultConfig())
+	client.DefaultCLIClient("", func(icfg *client.Config) client.Config {
+		if icfg == nil {
+			bchainInfo := types.DefaultBlockchainInfo()
+			constants := types.DefaultChainConstants()
+			return client.Config{
+				ChainName:    bchainInfo.Name,
+				NetworkName:  config.NetworkNameStandard,
+				ChainVersion: bchainInfo.ChainVersion,
+
+				CurrencyUnits:             constants.CurrencyUnits,
+				MinimumTransactionFee:     constants.MinimumTransactionFee,
+				DefaultTransactionVersion: constants.DefaultTransactionVersion,
+
+				BlockFrequencyInSeconds: int64(constants.BlockFrequency),
+				GenesisBlockTimestamp:   constants.GenesisBlock().Timestamp,
+			}
+		}
+		return *icfg
+	})
 }

pkg/client/defaultclient.go
@@ -2,11 +2,13 @@ package client
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"reflect"
 	"strings"
 
 	"github.com/rivine/rivine/build"
+	"github.com/rivine/rivine/pkg/daemon"
 	"github.com/rivine/rivine/types"
 	"github.com/spf13/cobra"
 )
@@ -23,29 +25,18 @@ const (
 
 // Config defines the configuration for the default (CLI) client.
 type Config struct {
-	Address               string
-	Name                  string
-	Version               build.ProtocolVersion // blockchain version (is NOT Rivine Protocol Version)
-	CurrencyCoinUnit      string
-	CurrencyUnits         types.CurrencyUnits
-	MinimumTransactionFee types.Currency
+	ChainName    string
+	NetworkName  string
+	ChainVersion build.ProtocolVersion
 
-	// version used to create new transactions
+	CurrencyUnits             types.CurrencyUnits
+	MinimumTransactionFee     types.Currency
 	DefaultTransactionVersion types.TransactionVersion
-}
 
-// DefaultConfig creates the default configuration for the default (CLI) client.
-func DefaultConfig() Config {
-	chainConstants := types.DefaultChainConstants()
-	return Config{
-		Address:                   "http://localhost:23110",
-		Name:                      "Rivine",
-		Version:                   build.Version,
-		CurrencyCoinUnit:          "ROC",
-		CurrencyUnits:             types.DefaultCurrencyUnits(),
-		MinimumTransactionFee:     chainConstants.MinimumTransactionFee,
-		DefaultTransactionVersion: chainConstants.DefaultTransactionVersion,
-	}
+	// These values aren't used for validation,
+	// but only in order to estimate progress with the syncing of your consensus.
+	BlockFrequencyInSeconds int64
+	GenesisBlockTimestamp   types.Timestamp
 }
 
 // Wrap wraps a generic command with a check that the command has been
@@ -127,24 +118,44 @@ var (
 	_CurrencyConvertor         CurrencyConvertor
 	_MinimumTransactionFee     types.Currency
 	_DefaultTransactionVersion types.TransactionVersion
+
+	_BlockFrequencyInSeconds int64
+	_GenesisBlockTimestamp   types.Timestamp
 )
 
-// DefaultCLIClient creates a new client using the given params as the default config,
-// and an optional flag-based system to overrride some.
-func DefaultCLIClient(cfg Config) {
-	_DefaultClient.name = cfg.Name
-	_DefaultClient.httpClient.RootURL = cfg.Address
-	_DefaultClient.version = cfg.Version
-	_CurrencyCoinUnit = cfg.CurrencyCoinUnit
-	_CurrencyUnits = cfg.CurrencyUnits
-	_MinimumTransactionFee = cfg.MinimumTransactionFee
-	_DefaultTransactionVersion = cfg.DefaultTransactionVersion
-
-	var err error
-	_CurrencyConvertor, err = NewCurrencyConvertor(_CurrencyUnits)
+func fetchConfigFromDaemon() *Config {
+	var constants daemon.SiaConstants
+	err := _DefaultClient.httpClient.GetAPI("/daemon/constants", &constants)
 	if err != nil {
-		Die("couldn't create currency convertor:", err)
+		log.Println("[ERROR] Failed to load constants from daemon: ", err)
+		return nil
 	}
+	return &Config{
+		ChainName:    constants.ChainInfo.Name,
+		NetworkName:  constants.ChainInfo.NetworkName,
+		ChainVersion: constants.ChainInfo.ChainVersion,
+		CurrencyUnits: types.CurrencyUnits{
+			OneCoin: constants.OneCoin,
+		},
+		MinimumTransactionFee:     constants.MinimumTransactionFee,
+		DefaultTransactionVersion: constants.DefaultTransactionVersion,
+		BlockFrequencyInSeconds:   int64(constants.BlockFrequency),
+		GenesisBlockTimestamp:     constants.GenesisTimestamp,
+	}
+}
+
+// DefaultCLIClient creates a new client for the given address.
+// The given address is used to connect to the client daemon, using its REST API.
+// configFunc has to be given and has two functions:
+// most importantly it serves to provide a default config, should the daemon/constants endpoint not be available,
+// and secondly it allows the CLi client to overwrite one or multiple properties.
+// Address input parameter defaults to "http://localhost:23110" if none is given.
+func DefaultCLIClient(address string, configFunc func(*Config) Config) {
+	if address == "" {
+		address = "http://localhost:23110"
+	}
+	_DefaultClient.httpClient.RootURL = address
+	_DefaultClient.name, _DefaultClient.version = "R?v?ne", build.Version // defaults for now, until we loaded real values
 
 	root := &cobra.Command{
 		Use:   os.Args[0],
@@ -152,11 +163,23 @@ func DefaultCLIClient(cfg Config) {
 		Long:  fmt.Sprintf("%s Client v", strings.Title(_DefaultClient.name)) + _DefaultClient.version.String(),
 		Run:   Wrap(consensuscmd),
 		PersistentPreRun: func(*cobra.Command, []string) {
+			// sanituze root URL
 			url, err := sanitizeURL(_DefaultClient.httpClient.RootURL)
 			if err != nil {
 				Die("invalid", strings.Title(_DefaultClient.name), "daemon RPC address", _DefaultClient.httpClient.RootURL, ":", err)
 			}
 			_DefaultClient.httpClient.RootURL = url
+
+			// configure client
+			cfg := configFunc(fetchConfigFromDaemon())
+			_DefaultClient.name = cfg.ChainName
+			_DefaultClient.version = cfg.ChainVersion
+			_CurrencyUnits = cfg.CurrencyUnits
+			_CurrencyConvertor = NewCurrencyConvertor(_CurrencyUnits)
+			_MinimumTransactionFee = cfg.MinimumTransactionFee
+			_DefaultTransactionVersion = cfg.DefaultTransactionVersion
+			_BlockFrequencyInSeconds = cfg.BlockFrequencyInSeconds
+			_GenesisBlockTimestamp = cfg.GenesisBlockTimestamp
 		},
 	}
 

pkg/client/consensuscmd.go
@@ -58,7 +58,7 @@ Target: %v
 		}
 		fmt.Printf(`Synced: %v
 Height: %v
-Progress (estimated): %.f%%
+Progress (estimated): %.2f%%
 `, YesNo(cg.Synced), cg.Height, estimatedProgress)
 	}
 }
@@ -67,9 +67,18 @@ Progress (estimated): %.f%%
 // Block height is estimated by calculating the minutes since a known block in
 // the past and dividing by 10 minutes (the block time).
 func EstimatedHeightAt(t time.Time) types.BlockHeight {
-	block5e4Timestamp := time.Date(2016, time.May, 11, 19, 33, 0, 0, time.UTC)
-	diff := t.Sub(block5e4Timestamp)
-	estimatedHeight := 5e4 + (diff.Minutes() / 10)
+	if _GenesisBlockTimestamp == 0 {
+		panic("GenesisBlockTimestamp is undefined")
+	}
+	return estimatedHeightBetween(int64(_GenesisBlockTimestamp), t.Unix(), _BlockFrequencyInSeconds)
+}
+
+func estimatedHeightBetween(from, to, blockFrequency int64) types.BlockHeight {
+	lifetimeInSeconds := to - from
+	if lifetimeInSeconds < blockFrequency {
+		return 0
+	}
+	estimatedHeight := float64(lifetimeInSeconds) / float64(blockFrequency)
 	return types.BlockHeight(estimatedHeight + 0.5) // round to the nearest block
 }
 

pkg/client/consensuscmd_test.go
@@ -9,41 +9,53 @@ import (
 
 // TestEstimatedHeightAt tests that the expectedHeightAt function correctly
 // estimates the blockheight (and rounds to the nearest block).
-func TestEstimatedHeightAt(t *testing.T) {
+func TestEstimatedHeightBetween(t *testing.T) {
 	tests := []struct {
-		t              time.Time
-		expectedHeight types.BlockHeight
+		From, To       int64 // timestamps in seconds
+		BlockFrequency int64 // duration in seconds
+		ExpectedHeight types.BlockHeight
 	}{
-		// Test on the same block that is used to estimate the height
+		// 0 or negative
 		{
-			time.Date(2016, time.May, 11, 19, 33, 0, 0, time.UTC),
-			5e4,
+			0, 0,
+			1, 0,
 		},
-		// 4 minutes later
 		{
-			time.Date(2016, time.May, 11, 19, 37, 0, 0, time.UTC),
-			5e4,
+			10, 10,
+			1, 0,
 		},
-		// 5 minutes later
 		{
-			time.Date(2016, time.May, 11, 19, 38, 0, 0, time.UTC),
-			5e4 + 1,
+			10, 5,
+			1, 0,
 		},
-		// 10 minutes later
+		// 1 (productive) hour
 		{
-			time.Date(2016, time.May, 11, 19, 43, 0, 0, time.UTC),
-			5e4 + 1,
+			0, 3600,
+			1, 3600,
 		},
-		// 1 day later
 		{
-			time.Date(2016, time.May, 12, 19, 33, 0, 0, time.UTC),
-			5e4 + 144,
+			0, 3600,
+			60, 60,
+		},
+		{
+			0, 3600,
+			120, 30,
+		},
+		// an example with more realistic numbers
+		{
+			// 7 days and 3 hours
+			time.Date(2018, 03, 2, 12, 0, 0, 0, time.Local).Unix(),
+			time.Date(2018, 03, 9, 15, 0, 0, 0, time.Local).Unix(),
+			// frequency of 10 minutes
+			600,
+			// expected height: roundUp((171h * 60 * 60) / 600)
+			1026,
 		},
 	}
-	for _, tt := range tests {
-		h := EstimatedHeightAt(tt.t)
-		if h != tt.expectedHeight {
-			t.Errorf("expected an estimated height of %v, but got %v", tt.expectedHeight, h)
+	for index, tt := range tests {
+		h := estimatedHeightBetween(tt.From, tt.To, tt.BlockFrequency)
+		if h != tt.ExpectedHeight {
+			t.Error(index, h, "!=", tt.ExpectedHeight, tt)
 		}
 	}
 }

pkg/client/parse.go
@@ -47,13 +47,13 @@ type CurrencyConvertor struct {
 // using the given currency units.
 //
 // See CurrencyConvertor for more information.
-func NewCurrencyConvertor(units types.CurrencyUnits) (CurrencyConvertor, error) {
+func NewCurrencyConvertor(units types.CurrencyUnits) CurrencyConvertor {
 	oneCoinStr := units.OneCoin.String()
 	precision := uint(len(oneCoinStr) - 1)
 	return CurrencyConvertor{
 		scalar:    units.OneCoin.Big(),
 		precision: precision,
-	}, nil
+	}
 }
 
 // ParseCoinString parses the given string assumed to be in the default unit,

pkg/client/parse_test.go
@@ -8,68 +8,8 @@ import (
 	"github.com/rivine/rivine/types"
 )
 
-func TestNewCurrencyConvertor(t *testing.T) {
-	testCases := []struct {
-		Input  types.CurrencyUnits
-		Output *CurrencyConvertor
-	}{
-		{
-			types.CurrencyUnits{
-				OneCoin: types.NewCurrency(new(big.Int).Exp(big.NewInt(10), big.NewInt(9), nil)),
-			},
-			&CurrencyConvertor{
-				scalar:    big.NewInt(1000000000),
-				precision: 9,
-			},
-		},
-		{
-			types.CurrencyUnits{
-				OneCoin: types.NewCurrency(new(big.Int).Exp(big.NewInt(10), big.NewInt(0), nil)),
-			},
-			&CurrencyConvertor{
-				scalar:    big.NewInt(1),
-				precision: 0,
-			},
-		},
-		{
-			types.CurrencyUnits{
-				OneCoin: types.NewCurrency(new(big.Int).Exp(big.NewInt(10), big.NewInt(24), nil)),
-			},
-			&CurrencyConvertor{
-				scalar:    new(big.Int).Exp(big.NewInt(10), big.NewInt(24), nil),
-				precision: 24,
-			},
-		},
-		{
-			types.CurrencyUnits{
-				OneCoin: types.NewCurrency(big.NewInt(1)),
-			},
-			&CurrencyConvertor{
-				scalar:    big.NewInt(1),
-				precision: 0,
-			},
-		},
-	}
-	for idx, testCase := range testCases {
-		cc, err := NewCurrencyConvertor(testCase.Input)
-		if testCase.Output == nil {
-			if err == nil {
-				t.Errorf("expected error for testCase #%d, but nothing received", idx)
-			}
-			continue
-		}
-
-		if testCase.Output.Cmp(cc) != 0 {
-			t.Errorf("#%d: %v != %v", idx, testCase.Output, cc)
-		}
-	}
-}
-
 func TestParseCoinStringInvalidStrings(t *testing.T) {
-	cc, err := NewCurrencyConvertor(types.DefaultCurrencyUnits())
-	if err != nil {
-		t.Fatal(err)
-	}
+	cc := NewCurrencyConvertor(types.DefaultCurrencyUnits())
 
 	testCases := []string{
 		"-1",
@@ -86,10 +26,7 @@ func TestParseCoinStringInvalidStrings(t *testing.T) {
 }
 
 func TestParseCoinStringValidStrings(t *testing.T) {
-	cc, err := NewCurrencyConvertor(types.DefaultCurrencyUnits())
-	if err != nil {
-		t.Fatal(err)
-	}
+	cc := NewCurrencyConvertor(types.DefaultCurrencyUnits())
 
 	testCases := []string{
 		"1",
@@ -117,32 +54,23 @@ func TestParseCoinStringValidStrings(t *testing.T) {
 }
 
 func TestParseCoinStringToCoinSmallValueString_E0(t *testing.T) {
-	cc, err := NewCurrencyConvertor(types.CurrencyUnits{
+	cc := NewCurrencyConvertor(types.CurrencyUnits{
 		OneCoin: types.NewCurrency(new(big.Int).Exp(big.NewInt(10), big.NewInt(0), nil)),
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 	testParseCoinStringToCoinSmallValueString(t, cc)
 }
 
 func TestParseCoinStringToCoinSmallValueString_E9(t *testing.T) {
-	cc, err := NewCurrencyConvertor(types.CurrencyUnits{
+	cc := NewCurrencyConvertor(types.CurrencyUnits{
 		OneCoin: types.NewCurrency(new(big.Int).Exp(big.NewInt(10), big.NewInt(9), nil)),
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 	testParseCoinStringToCoinSmallValueString(t, cc)
 }
 
 func TestParseCoinStringToCoinSmallValueString_E24(t *testing.T) {
-	cc, err := NewCurrencyConvertor(types.CurrencyUnits{
+	cc := NewCurrencyConvertor(types.CurrencyUnits{
 		OneCoin: types.NewCurrency(new(big.Int).Exp(big.NewInt(10), big.NewInt(24), nil)),
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 	testParseCoinStringToCoinSmallValueString(t, cc)
 }
 
@@ -182,32 +110,23 @@ func testParseCoinStringToCoinSmallValueString(t *testing.T, cc CurrencyConverto
 }
 
 func TestParseCoinStringToCoinBigValueString_E0(t *testing.T) {
-	cc, err := NewCurrencyConvertor(types.CurrencyUnits{
+	cc := NewCurrencyConvertor(types.CurrencyUnits{
 		OneCoin: types.NewCurrency(new(big.Int).Exp(big.NewInt(10), big.NewInt(0), nil)),
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 	testParseCoinStringToCoinBigValueString(t, cc)
 }
 
 func TestParseCoinStringToCoinBigValueString_E9(t *testing.T) {
-	cc, err := NewCurrencyConvertor(types.CurrencyUnits{
+	cc := NewCurrencyConvertor(types.CurrencyUnits{
 		OneCoin: types.NewCurrency(new(big.Int).Exp(big.NewInt(10), big.NewInt(9), nil)),
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 	testParseCoinStringToCoinBigValueString(t, cc)
 }
 
 func TestParseCoinStringToCoinBigValueString_E24(t *testing.T) {
-	cc, err := NewCurrencyConvertor(types.CurrencyUnits{
+	cc := NewCurrencyConvertor(types.CurrencyUnits{
 		OneCoin: types.NewCurrency(new(big.Int).Exp(big.NewInt(10), big.NewInt(24), nil)),
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 	testParseCoinStringToCoinBigValueString(t, cc)
 }
 

pkg/client/walletcmd_test.go
@@ -186,10 +186,6 @@ func TestParsePairedOutputs(t *testing.T) {
 
 func init() {
 	if _CurrencyConvertor == (CurrencyConvertor{}) {
-		var err error
-		_CurrencyConvertor, err = NewCurrencyConvertor(types.DefaultCurrencyUnits())
-		if err != nil {
-			panic(err)
-		}
+		_CurrencyConvertor = NewCurrencyConvertor(types.DefaultCurrencyUnits())
 	}
 }

pkg/daemon/commands.go
@@ -152,7 +152,7 @@ func SetupDefaultDaemon(cfg Config) {
 		fmt.Sprintf("enabled modules, see '%s modules' for more info", os.Args[0]))
 	root.Flags().BoolVarP(&cfg.AuthenticateAPI, "authenticate-api", "", cfg.AuthenticateAPI, "enable API password protection")
 	root.Flags().BoolVarP(&cfg.AllowAPIBind, "disable-api-security", "", cfg.AllowAPIBind, fmt.Sprintf("allow the daemon of %s to listen on a non-localhost address (DANGEROUS)", cfg.BlockchainInfo.Name))
-	root.Flags().StringVarP(&cfg.NetworkName, "network", "n", cfg.NetworkName, "the name of the network to which the daemon connects")
+	root.Flags().StringVarP(&cfg.BlockchainInfo.NetworkName, "network", "n", cfg.BlockchainInfo.NetworkName, "the name of the network to which the daemon connects")
 
 	// Parse cmdline flags, overwriting both the default values and the config
 	// file values.

commit a9b640e18046ad538e988782b140de54a16b6661

ensure CLI chain name is correct
fixes a bug introduced in 016931ead7f58333945bc8d01567fb8ea30e0444

cmd/rivinec/main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	client.DefaultCLIClient("", func(icfg *client.Config) client.Config {
+	bchainInfo := types.DefaultBlockchainInfo()
+	client.DefaultCLIClient("", bchainInfo.Name, func(icfg *client.Config) client.Config {
 		var networkName string
 		switch build.Release {
 		case "dev":
@@ -18,7 +19,6 @@ func main() {
 			networkName = "standard"
 		}
 		if icfg == nil {
-			bchainInfo := types.DefaultBlockchainInfo()
 			constants := types.DefaultChainConstants()
 			return client.Config{
 				ChainName:    bchainInfo.Name,

pkg/client/defaultclient.go
@@ -150,17 +150,20 @@ func fetchConfigFromDaemon() *Config {
 // most importantly it serves to provide a default config, should the daemon/constants endpoint not be available,
 // and secondly it allows the CLi client to overwrite one or multiple properties.
 // Address input parameter defaults to "http://localhost:23110" if none is given.
-func DefaultCLIClient(address string, configFunc func(*Config) Config) {
+func DefaultCLIClient(address, name string, configFunc func(*Config) Config) {
 	if address == "" {
 		address = "http://localhost:23110"
 	}
+	if name == "" {
+		name = "R?v?ne"
+	}
 	_DefaultClient.httpClient.RootURL = address
-	_DefaultClient.name, _DefaultClient.version = "R?v?ne", build.Version // defaults for now, until we loaded real values
+	_DefaultClient.name, _DefaultClient.version = name, build.Version // defaults for now, until we loaded real values from the daemon/client-callback
 
 	root := &cobra.Command{
 		Use:   os.Args[0],
-		Short: fmt.Sprintf("%s Client v", strings.Title(_DefaultClient.name)) + _DefaultClient.version.String(),
-		Long:  fmt.Sprintf("%s Client v", strings.Title(_DefaultClient.name)) + _DefaultClient.version.String(),
+		Short: fmt.Sprintf("%s Client", strings.Title(_DefaultClient.name)),
+		Long:  fmt.Sprintf("%s Client", strings.Title(_DefaultClient.name)),
 		Run:   Wrap(consensuscmd),
 		PersistentPreRun: func(*cobra.Command, []string) {
 			// sanituze root URL