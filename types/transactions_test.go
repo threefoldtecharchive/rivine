threefoldtecharchive/rivine

commit 59140e9737494558cf90392ba10881430ecc2663

remove any special-case logic for txn version 0
this makes v0 transactions as normal as any other transactions,
making it possible for a new chain to completely remove any trace of v0 transactions
in that chain

types/transactions.go
@@ -10,7 +10,6 @@ import (
 	"errors"
 	"io"
 
-	"github.com/rivine/rivine/build"
 	"github.com/rivine/rivine/crypto"
 	"github.com/rivine/rivine/encoding"
 )
@@ -162,17 +161,8 @@ var (
 // ID returns the id of a transaction, which is taken by marshalling all of the
 // fields except for the signatures and taking the hash of the result.
 func (t Transaction) ID() (id TransactionID) {
-	if t.Version == TransactionVersionZero {
-		ltd, err := newLegacyTransactionDataFromTransaction(t)
-		if build.DEBUG && err != nil {
-			panic(err)
-		}
-		// the legacy version does not include the transaction version
-		// as part of the crypto hash
-		return TransactionID(crypto.HashObject(ltd))
-	}
 	h := crypto.NewHash()
-	t.MarshalSia(h)
+	t.encodeTransactionDataAsIDInput(h)
 	h.Sum(id[:0])
 	return
 }
@@ -181,50 +171,52 @@ func (t Transaction) ID() (id TransactionID) {
 // which is calculated by hashing the concatenation of the CoinOutput
 // Specifier, all of the fields in the transaction (except the signatures),
 // and output index.
-func (t Transaction) CoinOutputID(i uint64) CoinOutputID {
-	if t.Version == TransactionVersionZero {
-		ltd, err := newLegacyTransactionDataFromTransaction(t)
-		if build.DEBUG && err != nil {
-			panic(err)
-		}
-		// the legacy version does not include the transaction version
-		// as part of the crypto hash
-		return CoinOutputID(crypto.HashAll(
-			SpecifierCoinOutput,
-			ltd,
-			i,
-		))
-	}
-	return CoinOutputID(crypto.HashAll(
-		SpecifierCoinOutput,
-		t,
-		i,
-	))
+func (t Transaction) CoinOutputID(i uint64) (id CoinOutputID) {
+	h := crypto.NewHash()
+	e := encoding.NewEncoder(h)
+	e.Encode(SpecifierCoinOutput)
+	t.encodeTransactionDataAsIDInput(h)
+	e.Encode(i)
+	h.Sum(id[:0])
+	return
 }
 
 // BlockStakeOutputID returns the ID of a BlockStakeOutput at the given index, which
 // is calculated by hashing the concatenation of the BlockStakeOutput Specifier,
 // all of the fields in the transaction (except the signatures), and output
 // index.
-func (t Transaction) BlockStakeOutputID(i uint64) BlockStakeOutputID {
-	if t.Version == TransactionVersionZero {
-		ltd, err := newLegacyTransactionDataFromTransaction(t)
-		if build.DEBUG && err != nil {
-			panic(err)
-		}
-		// the legacy version does not include the transaction version
-		// as part of the crypto hash
-		return BlockStakeOutputID(crypto.HashAll(
-			SpecifierBlockStakeOutput,
-			ltd,
-			i,
-		))
+func (t Transaction) BlockStakeOutputID(i uint64) (id BlockStakeOutputID) {
+	h := crypto.NewHash()
+	e := encoding.NewEncoder(h)
+	e.Encode(SpecifierBlockStakeOutput)
+	t.encodeTransactionDataAsIDInput(h)
+	e.Encode(i)
+	h.Sum(id[:0])
+	return
+}
+
+func (t Transaction) encodeTransactionDataAsIDInput(w io.Writer) error {
+	// get a controller registered or unknown controller
+	controller, exists := _RegisteredTransactionVersions[t.Version]
+	if !exists {
+		return ErrUnknownTransactionType
 	}
-	return BlockStakeOutputID(crypto.HashAll(
-		SpecifierBlockStakeOutput,
-		t,
-		i,
-	))
+	td := TransactionData{
+		CoinInputs:        t.CoinInputs,
+		CoinOutputs:       t.CoinOutputs,
+		BlockStakeInputs:  t.BlockStakeInputs,
+		BlockStakeOutputs: t.BlockStakeOutputs,
+		MinerFees:         t.MinerFees,
+		ArbitraryData:     t.ArbitraryData,
+		Extension:         t.Extension,
+	}
+	if transactionIDEncoder, ok := controller.(TransactionIDEncoder); ok {
+		// use binary encoded specialized for ID Input
+		return transactionIDEncoder.EncodeTransactionIDInput(w, td)
+	}
+	// use the default binary encoding, if the controller does not require specialized
+	// encoding logic for ID purposes
+	return controller.EncodeTransactionData(w, td)
 }
 
 // CoinOutputSum returns the sum of all the coin outputs in the
@@ -400,9 +392,6 @@ var (
 // IsValidTransactionVersion returns an error in case the
 // transaction version is not 0, and isn't registered either.
 func (v TransactionVersion) IsValidTransactionVersion() error {
-	if v == TransactionVersionZero {
-		return nil
-	}
 	if _, ok := _RegisteredTransactionVersions[v]; ok {
 		return nil
 	}

types/signatures.go
@@ -60,12 +60,12 @@ func Ed25519PublicKey(pk crypto.PublicKey) SiaPublicKey {
 
 // InputSigHash returns the hash of all fields in a transaction,
 // relevant to an input sig.
-func (t Transaction) InputSigHash(inputIndex uint64, extraObjects ...interface{}) crypto.Hash {
-	if t.Version == TransactionVersionZero {
-		return t.legacyInputSigHash(inputIndex, extraObjects...)
+func (t Transaction) InputSigHash(inputIndex uint64, extraObjects ...interface{}) (crypto.Hash, error) {
+	controller, exists := _RegisteredTransactionVersions[t.Version]
+	if !exists {
+		return crypto.Hash{}, ErrUnknownTransactionType
 	}
-
-	if hasher, ok := t.Extension.(InputSigHasher); ok {
+	if hasher, ok := controller.(InputSigHasher); ok {
 		// if extension implements InputSigHasher,
 		// use it here to sign the input with it
 		return hasher.InputSigHash(t, inputIndex, extraObjects...)
@@ -99,7 +99,7 @@ func (t Transaction) InputSigHash(inputIndex uint64, extraObjects ...interface{}
 
 	var hash crypto.Hash
 	h.Sum(hash[:0])
-	return hash
+	return hash, nil
 }
 
 func (t Transaction) legacyInputSigHash(inputIndex uint64, extraObjects ...interface{}) crypto.Hash {

types/signatures_test.go
@@ -33,8 +33,7 @@ func TestSigHash(t *testing.T) {
 		MinerFees:         []Currency{{}},
 		ArbitraryData:     []byte{'o', 't'},
 	}
-
-	_ = txn.InputSigHash(0)
+	txn.InputSigHash(0)
 }
 
 // TestSortedUnique probes the sortedUnique function.

types/transaction_controller.go
@@ -49,6 +49,8 @@ type (
 
 		// Extension is an optional field that can be used,
 		// in order to attach non-standard state to a transaction.
+		// It is used for data only, controller logic is all to be implemented
+		// as extended interfaces of the (transaction) controller.
 		Extension interface{} `json:"-"` // omitted from JSON
 	}
 
@@ -75,7 +77,14 @@ type (
 	// can optionally implement, in order to define custom Input signatures,
 	// overwriting the default input sig hash logic.
 	InputSigHasher interface {
-		InputSigHash(t Transaction, inputIndex uint64, extraObjects ...interface{}) crypto.Hash
+		InputSigHash(t Transaction, inputIndex uint64, extraObjects ...interface{}) (crypto.Hash, error)
+	}
+
+	// TransactionIDEncoder is an optional interface a transaction controller
+	// can implement, in order to use a different binary encoding for ID-generation purposes,
+	// instead of using the default binary encoding logic for that transaction (version).
+	TransactionIDEncoder interface {
+		EncodeTransactionIDInput(io.Writer, TransactionData) error
 	}
 )
 
@@ -210,4 +219,50 @@ func (ltc LegacyTransactionController) JSONDecodeTransactionData(b []byte) (Tran
 		return TransactionData{}, err
 	}
 	return ltd.TransactionData(), nil
+}
+
+// InputSigHash implements InputSigHasher.InputSigHash
+func (ltc LegacyTransactionController) InputSigHash(t Transaction, inputIndex uint64, extraObjects ...interface{}) (crypto.Hash, error) {
+	h := crypto.NewHash()
+	enc := encoding.NewEncoder(h)
+
+	enc.Encode(inputIndex)
+	if len(extraObjects) > 0 {
+		enc.EncodeAll(extraObjects...)
+	}
+	for _, ci := range t.CoinInputs {
+		enc.EncodeAll(ci.ParentID, legacyUnlockHashFromFulfillment(ci.Fulfillment.Fulfillment))
+	}
+	// legacy transactions encoded unlock hashes in pure form
+	enc.Encode(len(t.CoinOutputs))
+	for _, co := range t.CoinOutputs {
+		enc.EncodeAll(
+			co.Value,
+			legacyUnlockHashCondition(co.Condition.Condition),
+		)
+	}
+	for _, bsi := range t.BlockStakeInputs {
+		enc.EncodeAll(bsi.ParentID, legacyUnlockHashFromFulfillment(bsi.Fulfillment.Fulfillment))
+	}
+	// legacy transactions encoded unlock hashes in pure form
+	enc.Encode(len(t.BlockStakeOutputs))
+	for _, bso := range t.BlockStakeOutputs {
+		enc.EncodeAll(
+			bso.Value,
+			legacyUnlockHashCondition(bso.Condition.Condition),
+		)
+	}
+	enc.EncodeAll(
+		t.MinerFees,
+		t.ArbitraryData,
+	)
+
+	var hash crypto.Hash
+	h.Sum(hash[:0])
+	return hash, nil
+}
+
+// EncodeTransactionIDInput implements TransactionIDEncoder.EncodeTransactionIDInput
+func (ltc LegacyTransactionController) EncodeTransactionIDInput(w io.Writer, td TransactionData) error {
+	return ltc.EncodeTransactionData(w, td)
 }

types/unlockcondition.go
@@ -2152,7 +2152,10 @@ func signHashUsingSiaPublicKey(pk SiaPublicKey, inputIndex uint64, tx Transactio
 		if edSK.IsNil() {
 			return nil, crypto.ErrSecretNilKey
 		}
-		sigHash := tx.InputSigHash(inputIndex, extraObjects...)
+		sigHash, err := tx.InputSigHash(inputIndex, extraObjects...)
+		if err != nil {
+			return nil, err
+		}
 		sig := crypto.SignHash(sigHash, edSK)
 		return sig[:], nil
 
@@ -2184,8 +2187,11 @@ func verifyHashUsingSiaPublicKey(pk SiaPublicKey, inputIndex uint64, tx Transact
 			return crypto.ErrPublicNilKey
 		}
 		cryptoSig := crypto.Signature(edSig)
-		sigHash := tx.InputSigHash(inputIndex, extraObjects...)
-		err = crypto.VerifyHash(sigHash, edPK, cryptoSig)
+		var sigHash crypto.Hash
+		sigHash, err = tx.InputSigHash(inputIndex, extraObjects...)
+		if err == nil {
+			err = crypto.VerifyHash(sigHash, edPK, cryptoSig)
+		}
 
 	default:
 		err = ErrUnknownSignAlgorithmType

types/unlockcondition_test.go
@@ -1733,13 +1733,16 @@ func TestFulfillLegacyCompatibility(t *testing.T) {
 	}
 	for tidx, testCase := range testCases {
 		for idx, ci := range testCase.Transaction.CoinInputs {
-			sigHash := testCase.Transaction.InputSigHash(uint64(idx))
+			sigHash, err := testCase.Transaction.InputSigHash(uint64(idx))
+			if err != nil {
+				t.Error(tidx, idx, "unexpected error", err)
+			}
 			if bytes.Compare(testCase.ExpectedCoinInputSigHashes[idx][:], sigHash[:]) != 0 {
 				t.Error(tidx, idx, "invalid coin input sigh hash",
 					testCase.ExpectedCoinInputSigHashes[idx], "!=", sigHash)
 			}
 
-			err := ci.Fulfillment.IsStandardFulfillment(ValidationContext{})
+			err = ci.Fulfillment.IsStandardFulfillment(ValidationContext{})
 			if err != nil {
 				t.Error(tidx, idx, "unexpected error", err)
 			}
@@ -1753,13 +1756,16 @@ func TestFulfillLegacyCompatibility(t *testing.T) {
 			}
 		}
 		for idx, bsi := range testCase.Transaction.BlockStakeInputs {
-			sigHash := testCase.Transaction.InputSigHash(uint64(idx))
+			sigHash, err := testCase.Transaction.InputSigHash(uint64(idx))
+			if err != nil {
+				t.Error(tidx, idx, "unexpected error", err)
+			}
 			if bytes.Compare(testCase.ExpectedBlockStakeInputSigHashes[idx][:], sigHash[:]) != 0 {
 				t.Error(tidx, idx, "invalid bs input sigh hash",
 					testCase.ExpectedBlockStakeInputSigHashes[idx], "!=", sigHash)
 			}
 
-			err := bsi.Fulfillment.IsStandardFulfillment(ValidationContext{})
+			err = bsi.Fulfillment.IsStandardFulfillment(ValidationContext{})
 			if err != nil {
 				t.Error(tidx, idx, "unexpected error", err)
 			}

commit afecfd2ddc73cc10f36b28dced6fc507283189d2

fix output/txn ID computation
broken since commit 59140e9737494558cf90392ba10881430ecc2663

fixes threefoldfoundation/tfchain#134

might also be of relevance for #397 and threefoldfoundation/tfchain#133

types/transactions.go
@@ -201,22 +201,22 @@ func (t Transaction) encodeTransactionDataAsIDInput(w io.Writer) error {
 	if !exists {
 		return ErrUnknownTransactionType
 	}
-	td := TransactionData{
-		CoinInputs:        t.CoinInputs,
-		CoinOutputs:       t.CoinOutputs,
-		BlockStakeInputs:  t.BlockStakeInputs,
-		BlockStakeOutputs: t.BlockStakeOutputs,
-		MinerFees:         t.MinerFees,
-		ArbitraryData:     t.ArbitraryData,
-		Extension:         t.Extension,
-	}
 	if transactionIDEncoder, ok := controller.(TransactionIDEncoder); ok {
+		td := TransactionData{
+			CoinInputs:        t.CoinInputs,
+			CoinOutputs:       t.CoinOutputs,
+			BlockStakeInputs:  t.BlockStakeInputs,
+			BlockStakeOutputs: t.BlockStakeOutputs,
+			MinerFees:         t.MinerFees,
+			ArbitraryData:     t.ArbitraryData,
+			Extension:         t.Extension,
+		}
 		// use binary encoded specialized for ID Input
 		return transactionIDEncoder.EncodeTransactionIDInput(w, td)
 	}
 	// use the default binary encoding, if the controller does not require specialized
 	// encoding logic for ID purposes
-	return controller.EncodeTransactionData(w, td)
+	return t.MarshalSia(w)
 }
 
 // CoinOutputSum returns the sum of all the coin outputs in the

types/transactions_test.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/rivine/rivine/build"
 	"github.com/rivine/rivine/crypto"
 )
 
@@ -1790,6 +1791,178 @@ func TestIDComputationCompatibleWithLegacyIDs(t *testing.T) {
 	}
 }
 
+// unit test for https://github.com/threefoldfoundation/tfchain/issues/134
+func TestIDComputationCompatibleWithLegacyIDs_TFChain_Issue134(t *testing.T) {
+	type IDIndexPair struct {
+		Index    uint64
+		ParentID string
+	}
+	testCases := []struct {
+		JSONEncodedTransaction string
+		HexTransactionID       string
+		CoinParents            []IDIndexPair
+		BlockStakeParents      []IDIndexPair
+	}{
+		{
+			`{
+	"version": 1,
+	"data": {
+		"coininputs": null,
+		"blockstakeinputs": [{
+			"parentid": "782e4819d6e199856ba1bff3def5d7cc37ae2a0dabecb05359d6072156190d68",
+			"fulfillment": {
+				"type": 1,
+				"data": {
+					"publickey": "ed25519:95990ca3774de81309932302f74dfe9e540d6c29ca5cb9ee06e999ad46586737",
+					"signature": "70be2115b82a54170c94bf4788e2a6dd154a081f61e97999c2d9fcc64c41e7df2e8a8d4f82a57a04a1247b9badcb6bffbd238e9a6761dd59e5fef7ff6df0fc01"
+				}
+			}
+		}],
+		"blockstakeoutputs": [{
+			"value": "99",
+			"condition": {
+				"type": 1,
+				"data": {
+					"unlockhash": "01fdf10836c119186f1d21666ae2f7dc62d6ecc46b5f41449c3ee68aea62337dad917808e46799"
+				}
+			}
+		}],
+		"minerfees": null
+	}
+}`,
+			"af2e02682fb8423e4c9060d93b847b7e26b2a2b90f999b12a116605fa5358195",
+			nil,
+			[]IDIndexPair{
+				{0, "107a0a18a511cf1a6d4a2ac1f1a3626a7aa6004ef7aa21ac684b63b95594c6c1"},
+			},
+		},
+		{
+			`{
+	"version": 1,
+	"data": {
+		"coininputs": [{
+			"parentid": "5b907d6e4d34cdd825484d2f9f14445377fb8b4f8cab356a390a7fe4833a3085",
+			"fulfillment": {
+				"type": 1,
+				"data": {
+					"publickey": "ed25519:bd5e0e345d5939f5f9eb330084c7f0ffb8fc7fc5bdb07a94c304620eb4e2d99a",
+					"signature": "55dace7ccbc9cdd23220a8ef3ec09e84ce5c5acc202c5f270ea0948743ebf52135f3936ef7477170b4f9e0fe141a61d8312ab31afbf926a162982247e5d2720a"
+				}
+			}
+		}],
+		"coinoutputs": [{
+			"value": "1000000000",
+			"condition": {
+				"type": 1,
+				"data": {
+					"unlockhash": "010009a2b6a482da73204ccc586f6fab5504a1a69c0d316cdf828a476ae7c91c9137fd6f1b62bb"
+				}
+			}
+		}, {
+			"value": "8900000000",
+			"condition": {
+				"type": 1,
+				"data": {
+					"unlockhash": "01b81f9e02d6be3a7de8440365a7c799e07dedf2ccba26fd5476c304e036b87c1ab716558ce816"
+				}
+			}
+		}],
+		"minerfees": ["100000000"]
+	}
+}`,
+			"943778f4e2ecb5b2186e05c989b7c08e9d0199d419509725e166ac7f221600c7",
+			[]IDIndexPair{
+				{1, "733b266749141bad0900d7f9fe69dd511a98f7a6f4c78e66aa41af7f55650a15"},
+			},
+			nil,
+		},
+	}
+	for idx, testCase := range testCases {
+		var txn Transaction
+		err := txn.UnmarshalJSON([]byte(testCase.JSONEncodedTransaction))
+		if err != nil {
+			t.Error(idx, "failed to decode JSON txn", err)
+			continue
+		}
+		var txnID TransactionID
+		err = txnID.LoadString(testCase.HexTransactionID)
+		if err != nil {
+			t.Error(idx, "failed to decode hex encoded txnID", err)
+			continue
+		}
+		// test transaction ID
+		if txn.Version == TransactionVersionZero {
+			// ensure our legacy ID is correct for v0 transactions
+			legacyID := txn.LegacyID()
+			if legacyID != txnID {
+				t.Error(idx, "LegacyID", legacyID, "!=", txnID)
+			}
+		}
+		// sanity check, to ensure our expected ID is actually correct
+		v106ID := txn.V106ID()
+		if v106ID != txnID {
+			t.Error(idx, "V106ID", v106ID, "!=", txnID)
+		}
+		// ensure our computed ID is correct
+		transactionID := txn.ID()
+		if transactionID != txnID {
+			t.Error(idx, "TransactionID", transactionID, "!=", txnID)
+		}
+		// test coin parent IDs
+		for udx, parent := range testCase.CoinParents {
+			var parentID CoinOutputID
+			err = parentID.LoadString(parent.ParentID)
+			if err != nil {
+				t.Error(idx, udx, "failed to decode coin parentID", err)
+				continue
+			}
+			if txn.Version == TransactionVersionZero {
+				// ensure our legacy ID is correct for v0 transactions
+				legacyOutputID := txn.LegacyCoinOutputID(parent.Index)
+				if legacyOutputID != parentID {
+					t.Error(idx, udx, "LegacyCoinOutputID", legacyOutputID, "!=", parentID)
+				}
+			}
+			// sanity check, to ensure our expected ID is actually correct
+			v106OutputID := txn.V106CoinOutputID(parent.Index)
+			if v106OutputID != parentID {
+				t.Error(idx, udx, "V106CoinOutputID", v106OutputID, "!=", parentID)
+			}
+			// ensure our computed ID is correct
+			outputID := txn.CoinOutputID(parent.Index)
+			if outputID != parentID {
+				t.Error(idx, udx, "CoinOutputID", outputID, "!=", parentID)
+			}
+		}
+		// test block stake parent IDs
+		for udx, parent := range testCase.BlockStakeParents {
+			var parentID BlockStakeOutputID
+			err = parentID.LoadString(parent.ParentID)
+			if err != nil {
+				t.Error(idx, udx, "failed to decode block stake parentID", err)
+				continue
+			}
+			if txn.Version == TransactionVersionZero {
+				// ensure our legacy ID is correct for v0 transactions
+				legacyOutputID := txn.LegacyBlockStakeOutputID(parent.Index)
+				if legacyOutputID != parentID {
+					t.Error(idx, udx, "LegacyBlockStakeOutputID", legacyOutputID, "!=", parentID)
+				}
+			}
+			// sanity check, to ensure our expected ID is actually correct
+			v106OutputID := txn.V106BlockstakeOutputID(parent.Index)
+			if v106OutputID != parentID {
+				t.Error(idx, udx, "V106BlockstakeOutputID", v106OutputID, "!=", parentID)
+			}
+			// ensure our computed ID is correct
+			outputID := txn.BlockStakeOutputID(parent.Index)
+			if outputID != parentID {
+				t.Error(idx, udx, "BlockStakeOutputID", outputID, "!=", parentID)
+			}
+		}
+	}
+}
+
 // ID returns the id of a transaction, which is taken by marshalling all of the
 // fields except for the signatures and taking the hash of the result.
 func (t Transaction) LegacyID() TransactionID {
@@ -1824,6 +1997,63 @@ func (t Transaction) LegacyCoinOutputID(i uint64) CoinOutputID {
 		ltd.BlockStakeOutputs,
 		ltd.MinerFees,
 		ltd.ArbitraryData,
+		i,
+	))
+}
+
+// sanity checks for TestIDComputationCompatibleWithLegacyIDs_TFChain_Issue134
+func (t Transaction) V106ID() (id TransactionID) {
+	if t.Version == TransactionVersionZero {
+		ltd, err := newLegacyTransactionDataFromTransaction(t)
+		if build.DEBUG && err != nil {
+			panic(err)
+		}
+		// the legacy version does not include the transaction version
+		// as part of the crypto hash
+		return TransactionID(crypto.HashObject(ltd))
+	}
+	h := crypto.NewHash()
+	t.MarshalSia(h)
+	h.Sum(id[:0])
+	return
+}
+func (t Transaction) V106CoinOutputID(i uint64) CoinOutputID {
+	if t.Version == TransactionVersionZero {
+		ltd, err := newLegacyTransactionDataFromTransaction(t)
+		if build.DEBUG && err != nil {
+			panic(err)
+		}
+		// the legacy version does not include the transaction version
+		// as part of the crypto hash
+		return CoinOutputID(crypto.HashAll(
+			SpecifierCoinOutput,
+			ltd,
+			i,
+		))
+	}
+	return CoinOutputID(crypto.HashAll(
+		SpecifierCoinOutput,
+		t,
+		i,
+	))
+}
+func (t Transaction) V106BlockstakeOutputID(i uint64) BlockStakeOutputID {
+	if t.Version == TransactionVersionZero {
+		ltd, err := newLegacyTransactionDataFromTransaction(t)
+		if build.DEBUG && err != nil {
+			panic(err)
+		}
+		// the legacy version does not include the transaction version
+		// as part of the crypto hash
+		return BlockStakeOutputID(crypto.HashAll(
+			SpecifierBlockStakeOutput,
+			ltd,
+			i,
+		))
+	}
+	return BlockStakeOutputID(crypto.HashAll(
+		SpecifierBlockStakeOutput,
+		t,
 		i,
 	))
 }