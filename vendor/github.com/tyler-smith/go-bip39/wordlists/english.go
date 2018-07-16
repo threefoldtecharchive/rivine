2b37994bb98273506300b9d49365a3a56271d90e
Glen De Cauwsemaecker <contact@glendc.com>
2018-07-16 09:39:26 +0200

    use tyler-smith/go-bip39 lib instead of our fork
    master repo of go-bip39 (by Tyler Smith) became active once again,
    our changes have been merged into it as well,
    therefore it seems like a sensible choice to remove our fork and
    start using the master repo

--- vendor/github.com/tyler-smith/go-bip39/wordlists/english.go
+++ vendor/github.com/tyler-smith/go-bip39/wordlists/english.go
@@ -1,4 +1,4 @@
-package bip39
+package wordlists
 
 import (
 	"fmt"
@@ -6,29 +6,21 @@ import (
 	"strings"
 )
 
-var _ReverseEnglishWordMap = map[string]int{}
-
 func init() {
-	for i, v := range _EnglishWordList {
-		_ReverseEnglishWordMap[v] = i
-	}
-
 	// Ensure word list is correct
 	// $ wget https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt
 	// $ crc32 english.txt
 	// c1dbd296
-	checksum := crc32.ChecksumIEEE([]byte(EnglishWordListString))
+	checksum := crc32.ChecksumIEEE([]byte(english))
 	if fmt.Sprintf("%x", checksum) != "c1dbd296" {
-		panic("englishWordList checksum invalid")
+		panic("english checksum invalid")
 	}
 }
 
-// Language-specific wordlists
-var _EnglishWordList = strings.Split(strings.TrimSpace(EnglishWordListString), "\n")
-
-// EnglishWordListString source can be found
-// at https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt
-const EnglishWordListString = `abandon
+// English is a slice of mnemonic words taken from the bip39 specification
+// https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt
+var English = strings.Split(strings.TrimSpace(english), "\n")
+var english = `abandon
 ability
 able
 about
