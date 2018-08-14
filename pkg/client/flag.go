ee770ff34f35acabcc0315eb5f3dfaa165ea8710
Glen De Cauwsemaecker <contact@glendc.com>
2018-08-14 20:05:04 +0200

    turn cli client into a struct
    + eliminating all globals in the pkg/client pkg
    + opening the possibility for a rivine-based chain cli to add its own specialized commands

--- pkg/client/flag.go
+++ pkg/client/flag.go
@@ -8,43 +8,60 @@ import (
 	"github.com/spf13/pflag"
 )
 
+func currencyFlag(ccFactory currencyConvertorFactory, value *types.Currency) *coinFlag {
+	if ccFactory == nil {
+		panic("no currency convertor factory given")
+	}
+	if value == nil {
+		panic("nil currency value reference given")
+	}
+	return &coinFlag{
+		ccFactory: ccFactory,
+		value:     value,
+	}
+}
+
 type coinFlag struct {
-	str string
+	ccFactory currencyConvertorFactory
+	value     *types.Currency
+}
+
+type currencyConvertorFactory interface {
+	CreateCurrencyConvertor() CurrencyConvertor
 }
 
 // String implements pflag.Value.String
-func (c coinFlag) String() string {
-	return c.str
+func (c *coinFlag) String() string {
+	cc := c.ccFactory.CreateCurrencyConvertor()
+	return cc.ToCoinString(*c.value)
 }
 
 // Set implements pflag.Value.Set
 func (c *coinFlag) Set(s string) error {
-	c.str = s
+	cc := c.ccFactory.CreateCurrencyConvertor()
+	value, err := cc.ParseCoinString(s)
+	if err != nil {
+		return err
+	}
+	*c.value = value
 	return nil
 }
 
 // Type implements pflag.Value.Type
-func (c coinFlag) Type() string {
+func (c *coinFlag) Type() string {
 	return "Coin"
-}
-
-func parseCoinArg(str string) types.Currency {
-	amount, err := _CurrencyConvertor.ParseCoinString(str)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, _CurrencyConvertor.CoinArgDescription("amount"))
-		DieWithExitCode(ExitCodeUsage, "failed to parse coin-typed argument: ", err)
-		return types.Currency{}
-	}
-	return amount
-}
-
-func (c coinFlag) Amount() types.Currency {
-	if c.str == "" {
-		return types.Currency{}
-	}
-	return parseCoinArg(c.str)
 }
 
 var (
 	_ pflag.Value = (*coinFlag)(nil)
 )
+
+func parseCoinArg(cc CurrencyConvertor, str string) types.Currency {
+	amount, err := cc.ParseCoinString(str)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, cc.CoinArgDescription("amount"))
+		DieWithExitCode(ExitCodeUsage, "failed to parse coin-typed argument: ", err)
+		return types.Currency{}
+	}
+	return amount
+}
