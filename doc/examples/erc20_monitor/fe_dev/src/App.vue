abeba4050c322cce5714853a88789c5903de5c75
LeeSmet <lee.smet@hotmail.com>
2018-11-09 15:27:22 +0100

    Initial demo setup

---
+++ doc/examples/erc20_monitor/fe_dev/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TokenBalance name="ERC20-TTFT" contract-addr="0xe392e4dadeef43f326370d9e9b9f4990bf2636f5" />
+  </div>
+</template>
+
+<script>
+import TokenBalance from "./components/TokenBalance.vue";
+
+export default {
+  name: "app",
+  components: {
+    TokenBalance
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
