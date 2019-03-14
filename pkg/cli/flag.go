11e50ff83762cc879239f89577f2a8f278f12a2c
Rob Van Mieghem <robvanmieghem@gmail.com>
2019-03-14 09:21:20 +0100

    Add a NetAddressArrayFlag type

--- pkg/cli/flag.go
+++ pkg/cli/flag.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/spf13/pflag"
+	"github.com/threefoldtech/rivine/modules"
 )
 
 const (
@@ -107,6 +108,7 @@ type (
 	// EncodingType defines an enum, to represent all encoding options.
 	EncodingType uint8
 
+	// ArbitraryDataFlag is used to pass arbitrary data to transactions
 	ArbitraryDataFlag struct {
 		arbitraryData *[]byte
 	}
@@ -261,6 +263,57 @@ func (flag *ArbitraryDataFlag) String() string {
 
 	s := strconv.Quote(string(byteArray[:]))
 	return strings.Trim(s, "\"")
+}
+
+// NetAddressArrayFlagVar defines a []modules.NetAddress flag with specified name and usage string.
+// The argument s points to a []modules.NetAddress variable in which to store the validated values of the flags.
+// The value of each argument will not try to be separated by comma, each value has to be defined as a separate flag (using the same name).
+func NetAddressArrayFlagVar(f *pflag.FlagSet, s *[]modules.NetAddress, name string, usage string) {
+	f.Var(&netAddressArray{array: s}, name, usage)
+}
+
+// NetAddressArrayFlagVarP defines a []modules.NetAddress flag with specified name, shorthand and usage string.
+// The argument s points to a []modules.NetAddress variable in which to store the validated values of the flags.
+// The value of each argument will not try to be separated by comma, each value has to be defined as a separate flag (using the same name or shorthand).
+func NetAddressArrayFlagVarP(f *pflag.FlagSet, s *[]modules.NetAddress, name, shorthand string, usage string) {
+	f.VarP(&netAddressArray{array: s}, name, shorthand, usage)
+}
+
+type netAddressArray struct {
+	array   *[]modules.NetAddress
+	changed bool
+}
+
+// Set implements pflag.Value.Set
+func (flag *netAddressArray) Set(val string) error {
+	if !flag.changed {
+		*flag.array = make([]modules.NetAddress, 0)
+		flag.changed = true
+	}
+	na := modules.NetAddress(val)
+	err := na.IsStdValid()
+	if err != nil {
+		return fmt.Errorf("invalid network address %v: %v", val, err)
+	}
+	*flag.array = append(*flag.array, na)
+	return nil
+}
+
+// Type implements pflag.Value.Type
+func (flag *netAddressArray) Type() string {
+	return "NetAddressArray"
+}
+
+// String implements pflag.Value.String
+func (flag *netAddressArray) String() string {
+	if flag.array == nil || len(*flag.array) == 0 {
+		return ""
+	}
+	var str string
+	for _, na := range *flag.array {
+		str += string(na) + ","
+	}
+	return str[:len(str)-1]
 }
 
 var computeTimeNow = func() time.Time {
--- pkg/cli/flag_test.go
+++ pkg/cli/flag_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/threefoldtech/rivine/modules"
 	"github.com/threefoldtech/rivine/types"
 )
 
@@ -363,6 +364,40 @@ func Test_arbitraryDataFlag(t *testing.T) {
 				t.Errorf("String() = %v, want %v", got, tt.input)
 			}
 		})
+	}
+}
+
+func TestNetAddressArrayFlag(t *testing.T) {
+	var s []modules.NetAddress
+	flag := netAddressArray{array: &s}
+
+	str := flag.String()
+	if len(str) != 0 {
+		t.Fatal("unexpected stringified NetAddressArrayFlag: ", str)
+	}
+
+	flag.Set("127.0.0.1:23112")
+	expectedStr := "127.0.0.1:23112"
+	str = flag.String()
+	if expectedStr != str {
+		t.Fatal("stringified NetAddressArrayFlag unexpected:", expectedStr, "!=", str)
+	}
+
+	flag.Set("localhost:23122")
+	expectedStr = "127.0.0.1:23112,localhost:23122"
+	str = flag.String()
+	if expectedStr != str {
+		t.Fatal("stringified NetAddressArrayFlag unexpected:", expectedStr, "!=", str)
+	}
+
+	expectedStrs := []modules.NetAddress{"127.0.0.1:23112", "localhost:23122"}
+	if len(s) != len(expectedStrs) {
+		t.Fatal("unexpected NetAddressArrayFlag result:", s)
+	}
+	for i, addr := range s {
+		if addr != expectedStrs[i] {
+			t.Errorf("unexpected NetAddressArrayFlag result value #%d: %s != %s", i+1, addr, expectedStrs[i])
+		}
 	}
 }
 
