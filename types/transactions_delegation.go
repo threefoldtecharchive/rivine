5f4f99a8288d75b270b2da78db395aa5db396a9c
LeeSmet <lee.smet@hotmail.com>
2019-04-09 10:26:00 +0200

    Add basic transaction interface methods to delegation tx

--- types/transactions_delegation.go
+++ types/transactions_delegation.go
@@ -1,5 +1,13 @@
 package types
 
+import (
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/threefoldtech/rivine/pkg/encoding/rivbin"
+)
+
 // These Specifiers are used internally when calculating a type's ID. See
 // Specifier for more details.
 var (
@@ -16,6 +24,9 @@ type (
 	// DelegationTransaction defines the transaction (with version 0x03)
 	// used to allow a third party to use these blockstakes to create new blocks
 	DelegationTransaction struct {
+		CoinInputs    []CoinInput
+		CoinOutputs   []CoinOutput
+		ArbitraryData []byte
 		// Reference unlocks a blockstake output to prove ownership, but does not consume it
 		Reference BlockStakeInput
 		// Delegation is the condition which needs to be unlocked to use the delegated blockstakes
@@ -30,3 +41,122 @@ type (
 		Delegation BlockStakeOutput
 	}
 )
+
+// DelegationTransactionFromTransaction creates a DelegationTransaction, using a regular in-memory rivine transaction
+func DelegationTransactionFromTransaction(tx Transaction) (DelegationTransaction, error) {
+	if tx.Version != TransactionVersionDelegation {
+		return DelegationTransaction{}, fmt.Errorf(
+			"delegation transaction requires tx version %d", TransactionVersionDelegation,
+		)
+	}
+	return DelegationTransactionFromTransactionData(TransactionData{
+		CoinInputs:        tx.CoinInputs,
+		CoinOutputs:       tx.CoinOutputs,
+		BlockStakeInputs:  tx.BlockStakeInputs,
+		BlockStakeOutputs: tx.BlockStakeOutputs,
+		MinerFees:         tx.MinerFees,
+		ArbitraryData:     tx.ArbitraryData,
+		Extension:         tx.Extension,
+	})
+}
+
+// DelegationTransactionFromTransactionData creates a DelegationTransaction,
+// using the TransactionData from a regular in-memory rivine transaction.
+func DelegationTransactionFromTransactionData(txData TransactionData) (DelegationTransaction, error) {
+	// validate transaction data
+
+	// need at least 1 coin input to fund the transaction
+	if len(txData.CoinInputs) == 0 {
+		return DelegationTransaction{}, errors.New("need at least one coin input for a delegate  transaction")
+	}
+
+	// need minerfees
+	if len(txData.MinerFees) == 0 {
+		return DelegationTransaction{}, errors.New("transaction fees must be paid for a delegation transaction")
+	}
+
+	// no blockstake input and outputs allowed, can't move block stakes. The block stakes to delegate are identified
+	// in the transaction data
+	if len(txData.BlockStakeOutputs) != 0 || len(txData.BlockStakeInputs) != 0 {
+		return DelegationTransaction{}, errors.New("no blockstake outputs allowed for a delegate transaction")
+	}
+
+	tx := DelegationTransaction{}
+
+	if txData.Extension != nil {
+		extensionData, ok := txData.Extension.(*DelegationTransactionExtension)
+		if !ok {
+			return tx, errors.New("invalid extension data for a block creation transaction")
+		}
+
+		tx.Reference = extensionData.Reference
+		tx.Delegation = extensionData.Delegation
+	}
+
+	return tx, nil
+}
+
+// TransactionData returns this DelegationTransaction
+// as regular rivine transaction data.
+func (dtx *DelegationTransaction) TransactionData() TransactionData {
+	txData := TransactionData{
+		ArbitraryData: dtx.ArbitraryData,
+		CoinInputs:    dtx.CoinInputs,
+		CoinOutputs:   dtx.CoinOutputs,
+		Extension: &DelegationTransactionExtension{
+			Reference:  dtx.Reference,
+			Delegation: dtx.Delegation,
+		},
+	}
+	return txData
+}
+
+// Transaction returns this DelegationTransaction
+// as regular rivine transaction, using TransactionVersionDelegation as the type.
+func (dtx *DelegationTransaction) Transaction() Transaction {
+	tx := Transaction{
+		Version:       TransactionVersionDelegation,
+		ArbitraryData: dtx.ArbitraryData,
+		CoinInputs:    dtx.CoinInputs,
+		CoinOutputs:   dtx.CoinOutputs,
+		Extension: &DelegationTransaction{
+			Reference:  dtx.Reference,
+			Delegation: dtx.Delegation,
+		},
+	}
+	return tx
+}
+
+// MarshalSia implements SiaMarshaler.MarshalSia,
+// alias of MarshalRivine for backwards-compatibility reasons.
+func (dtx DelegationTransaction) MarshalSia(w io.Writer) error {
+	return dtx.MarshalRivine(w)
+}
+
+// UnmarshalSia implements SiaUnmarshaler.UnmarshalSia,
+// alias of UnmarshalRivine for backwards-compatibility reasons.
+func (dtx *DelegationTransaction) UnmarshalSia(r io.Reader) error {
+	return dtx.UnmarshalRivine(r)
+}
+
+// MarshalRivine implements RivineMarshaler.MarshalRivine
+func (dtx DelegationTransaction) MarshalRivine(w io.Writer) error {
+	return rivbin.NewEncoder(w).EncodeAll(
+		dtx.CoinInputs,
+		dtx.CoinOutputs,
+		dtx.ArbitraryData,
+		dtx.Reference,
+		dtx.Delegation,
+	)
+}
+
+// UnmarshalRivine implements RivineUnmarshaler.UnmarshalRivine
+func (dtx *DelegationTransaction) UnmarshalRivine(r io.Reader) error {
+	return rivbin.NewDecoder(r).DecodeAll(
+		&dtx.CoinInputs,
+		&dtx.CoinOutputs,
+		&dtx.ArbitraryData,
+		&dtx.Reference,
+		&dtx.Delegation,
+	)
+}
