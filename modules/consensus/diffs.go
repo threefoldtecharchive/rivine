threefoldtecharchive/rivine

commit 352f22c25b371bc62fdec90cfcd0821e65cf30cf

Use blockstake output from correct fork
When validating if a tx is indeed a block creation tx on an inactive
fork, go back from the given block so we are certain we load the correct
transaction for validation

modules/consensus/diffs.go
@@ -255,11 +255,23 @@ func (cs *ConsensusSet) isBlockCreatingTx(txIdx int, block types.Block) bool {
 
 	// this might be block creation transaction, so do a more expensive check to see
 	// if we indeed spend the output indicated in the block header
-	t, exists := cs.TransactionAtShortID(types.NewTransactionShortID(
-		block.Header().POBSOutput.BlockHeight, uint16(block.Header().POBSOutput.TransactionIndex)))
+
+	// Note that we can't expect the block to extend the active fork, if we do
+	// we won't be able to recover forks
+	height, exists := cs.BlockHeightOfBlock(block)
 	if !exists {
 		return false
 	}
+	refBlock, exists := cs.FindParentBlock(block, height-block.Header().POBSOutput.BlockHeight)
+	if !exists {
+		return false
+	}
+	if block.Header().POBSOutput.TransactionIndex >= uint64(len(refBlock.Transactions)) ||
+		block.Header().POBSOutput.OutputIndex >= uint64(len(refBlock.Transactions[block.Header().POBSOutput.TransactionIndex].BlockStakeOutputs)) {
+		return false
+	}
+
+	t := refBlock.Transactions[block.Header().POBSOutput.TransactionIndex]
 
 	if uint64(len(t.BlockStakeOutputs)) > block.Header().POBSOutput.OutputIndex &&
 		t.BlockStakeOutputID(block.Header().POBSOutput.OutputIndex) ==

commit e11f8fa4fa126b42dc39a7cfaf4a40b9b1b08abb

Fix validation on block creation txes
Fix bug introduced in 352f22c25b371bc62fdec90cfcd0821e65cf30cf, which
caused all new blocks to be rejected

modules/consensus/diffs.go
@@ -258,10 +258,22 @@ func (cs *ConsensusSet) isBlockCreatingTx(txIdx int, block types.Block) bool {
 
 	// Note that we can't expect the block to extend the active fork, if we do
 	// we won't be able to recover forks
-	height, exists := cs.BlockHeightOfBlock(block)
+	//
+	// Also, the block under validation can not be expected to be in the cs yet,
+	// so we first load its parent, then the height of the parent, then work
+	// from there
+	parent, exists := cs.FindParentBlock(block, 1)
+	if !exists {
+		// can this happen?
+		return false
+	}
+	height, exists := cs.BlockHeightOfBlock(parent)
 	if !exists {
 		return false
 	}
+	// height points at our parent block right now, so increment by one to
+	// account for that
+	height++
 	refBlock, exists := cs.FindParentBlock(block, height-block.Header().POBSOutput.BlockHeight)
 	if !exists {
 		return false