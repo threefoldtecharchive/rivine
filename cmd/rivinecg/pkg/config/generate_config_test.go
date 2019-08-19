3a26d3b51acf4b5345953ef194c4a8987eda5f83
dylanverstraete <dylan_verstraete1@hotmail.com>
2019-08-19 10:47:56 +0200

    adds tests for loading and generating blockchain

--- cmd/rivinecg/pkg/config/generate_config_test.go
+++ cmd/rivinecg/pkg/config/generate_config_test.go
@@ -21,28 +21,79 @@ func TestGenerateConfigFileWithUnknownFileType(t *testing.T) {
 // TestGenerateAndLoadConfigFile creates a default config file with some default values using 2 different file types.
 // Check if the file is created properly and removes it after this test ends
 // Then it loads this configuration, validates and fills in some default values if these are not provided.
-// func TestGenerateAndLoadConfigFile(t *testing.T) {
-// 	for _, typ := range []string{"yaml", "json"} {
-// 		filepath := path.Join(os.TempDir(), "blockchaincfg."+typ)
-// 		err := GenerateConfigFile(filepath)
-// 		if err != nil {
-// 			t.Errorf("Error occured: %s", err)
-// 		}
+func TestGenerateAndLoadConfigFile(t *testing.T) {
+	for _, typ := range []string{"yaml", "json"} {
+		filepath := path.Join(os.TempDir(), "blockchaincfg."+typ)
+		err := GenerateConfigFile(filepath)
+		if err != nil {
+			t.Errorf("Error occured: %s", err)
+		}
 
-// 		err = LoadConfigFile(filepath)
-// 		if err != nil {
-// 			t.Errorf("Error occured loading file: %s", err)
-// 		}
-// 		_, err = os.Stat(filepath)
-// 		if os.IsNotExist(err) {
-// 			t.Errorf("File is not created, %s", err)
-// 		}
-// 		if err != nil {
-// 			t.Errorf("Error occured: %s", err)
-// 		}
-// 		err = os.Remove(filepath)
-// 		if err != nil {
-// 			t.Errorf("Error occured removing file: %s", err)
-// 		}
-// 	}
-// }
+		file, err := os.Open(filepath)
+		if err != nil {
+			t.Errorf("Error occured: %s", err)
+		}
+
+		_, err = loadConfig("."+typ, file)
+		if err != nil {
+			t.Errorf("Error occured loading file: %s", err)
+		}
+		_, err = os.Stat(filepath)
+		if os.IsNotExist(err) {
+			t.Errorf("File is not created, %s", err)
+		}
+		if err != nil {
+			t.Errorf("Error occured: %s", err)
+		}
+		err = os.Remove(filepath)
+		if err != nil {
+			t.Errorf("Error occured removing file: %s", err)
+		}
+	}
+}
+
+// TestGenerateAndLoadConfigFile creates a default config file with some default values using 2 different file types.
+// Check if the file is created properly and removes it after this test ends
+// Then it loads this configuration, validates and fills in some default values if these are not provided.
+func TestGenerateAndLoadConfigFileAndGetTemplateRepoAndGenerateBlockchainCode(t *testing.T) {
+	for _, typ := range []string{"yaml", "json"} {
+		filepath := path.Join(os.TempDir(), "blockchaincfg."+typ)
+		err := GenerateConfigFile(filepath)
+		if err != nil {
+			t.Errorf("Error occured: %s", err)
+		}
+
+		file, err := os.Open(filepath)
+		if err != nil {
+			t.Errorf("Error occured: %s", err)
+		}
+
+		conf, err := loadConfig("."+typ, file)
+		if err != nil {
+			t.Errorf("Error occured loading file: %s", err)
+		}
+
+		dirpath := path.Join(os.TempDir(), "tmpbctest")
+		commitHash, err := getTemplateRepo(conf.Template.Repository.Owner, conf.Template.Repository.Repo, conf.Template.Version, dirpath)
+		if err != nil {
+			t.Errorf("Error occured fetching template repo: %s", err)
+		}
+
+		err = generateBlockchainTemplate(dirpath, commitHash, conf)
+		if err != nil {
+			t.Errorf("Error occured generating blockchain code: %s", err)
+		}
+
+		_, err = os.Stat(filepath)
+		if os.IsNotExist(err) {
+			t.Errorf("File is not created, %s", err)
+		}
+		if err != nil {
+			t.Errorf("Error occured: %s", err)
+		}
+		err = os.Remove(filepath)
+		if err != nil {
+			t.Errorf("Error occured removing file: %s", err)
+		}
+	}
+}
